67b909acf1bc8465ffb9a885a854b77b5a79ce97
Nitin Divate <nitin.divate@equensWorldline.com>
2020-04-07 15:59:49 +0200

    Update UBS subissuer maskparams, and INFO REFUSAL Template change.

--- acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U7G/20R1_UBS_Setup/update/u7g_bo_4_update_dateFormat_templates_UBS.sql
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U7G/20R1_UBS_Setup/update/u7g_bo_4_update_dateFormat_templates_UBS.sql
@@ -1,9 +1,11 @@
+
 USE U7G_ACS_BO;
 
 SET @subIssuerNameAndLabel = 'UBS Switzerland AG';
 SET @subIssuerCode = '23000';
 SET @dateFormat = 'DD.MM.YYYY';
+SET @maskParam = '•,0,4';
 
 UPDATE `SubIssuer`
-SET `dateFormat` = @dateFormat
+SET `dateFormat` = @dateFormat, `maskParams` = @maskParam 
 WHERE `code` = @subIssuerCode AND `name` = @subIssuerNameAndLabel;
--- acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U7G/20R1_UBS_Setup/update/u7g_bo_6_update_templates_UBS.sql
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U7G/20R1_UBS_Setup/update/u7g_bo_6_update_templates_UBS.sql
@@ -1041,255 +1041,248 @@ UPDATE CustomComponent SET value = '
 
 SET @layoutId = (SELECT id FROM `CustomPageLayout` WHERE `DESCRIPTION` like CONCAT('INFO Refusal Page (', @BankB, ')%') );
 
-UPDATE CustomComponent SET value = '
-<style>
-        #main-container {
-                font-family: FrutigerforUBSWeb;
-                max-width: 600px;
-                max-height: 600px;
-                margin-left: auto;
-                margin-right: auto;
-        }
-        #main-container   #pageHeader {
-                width: 100%;
-                height: 74px;
-		}
-        #main-container   #pageHeaderLeft {
-                width: 20%;
-                float: left;
-                padding-left: 16px;
-                padding-top: 16px;
-		}
-		#main-container   #pageHeaderRight {
-                width: 25%;
-                float: right;
-                text-align: right;
-                padding-top: 16px;
-		}
-		#main-container  #issuerLogo {
-                max-height: 64px;
-                max-width:100%;
-                padding-left: 0px;
-                padding-right: 0px;
-		}
-		#main-container   #networkLogo {
-                max-height: 100%;
-                max-width: 100%;
-		}
-		#main-container  #content {
-                text-align:left;
-                margin-left: 3em;
-		}
-        div#leftMenuLayout {
-                font-size: 16px;
-                letter-spacing: 0;
-                line-height: 24px;
-                margin-top: 10px;
-                padding-right: 0px;
-                padding-left: 0px;
-        }
-        div#leftMenuLayout span.ng-binding {
-                font-family: FrutigerforUBSWeb;
-                font-weight: normal;
-                font-size: 16px;
-                color: #1C1C1C;
-                letter-spacing: 0;
-                line-height: 24px;
-        }
-        div#leftMenuLayout span.custom-text.ng-binding {
-                font-family: Frutiger55Roman;
-                font-weight: normal;
-                font-size: 16px;
-                color: #646464;
-                letter-spacing: 0;
-                line-height: 24px;
-        }
-        #main-container .menu-title {
-                display:none;
-        }
-        #main-container .menu-elements {
-                padding-right:10%;
-        }
-        #main-container .side-menu .menu-elements {
-                line-height: 12px;
-        }
-        #main-container #contentBottom {
-                padding-left: 32%;
-        }
-        #message-container {
-                display: inline-block;
-                margin-top: 5px;
-                position: relative;
-                width: 196px;
-		}
-        div#message-content {
-                text-align: center;
-                background-color: inherit;
-                padding-bottom: 5px;
-                background-color:#F7E1DF;
-                font-family: FrutigerforUBSWeb;
-                font-size:12px;
-                color: #E0B519;
-        }
-        span#info-icon {
-                font-size: 1em;
-                top: 5px;
-                left: 5px;
-                float: left;
-                margin-right: 5px;
-        }
-        #message {
-                font-family: FrutigerforUBSWeb;
-                color: #1C1C1C;
-                font-size:12px;
-                line-height: 16px;
-                text-align:center;
-        }
-        span#message {
-                font-size:10px;
-                width: 100%;
-        }
-        div.message-button {
-                padding-top: 0px;
-                display: none;
-        }
-        div#spinner-row .spinner div div{
-                background: #E0B519!important;
-        }
-		#main-container   div#footer {
-			    background-image:none;
-			    height:44px;
-		}
-		#main-container   #footer {
-                width:100%;
-                clear:both;
-		}
-		#main-container   #footer:after {
-                content: "";
-                height:100%;
-                display: table;
-                clear: both;
-                padding-bottom: 0.5em;
-		}
-		#main-container  #footer .help-area {
-                display: flex;
-                flex-direction: row;
-                padding-top:24px;
-		}
-        #main-container  .help-link {
-                font-size: 14px;
-                line-height: 20px;
-                width: 50%;
-                order: 2;
-                text-align: left;
-                color:#007099;
-		}
-        #main-container #helpButton  button span{
-                color:#007099;
-                background-color: #f7f7f7;
-        }
-        #main-container #helpButton  button {
-                border-style: none;
-                padding:0px
-        }
-        #main-container   #helpButton span.fa-info{
-                display:none;
-        }
-		#main-container   .col-lg-4 {
-                width: 100%;
-                margin-bottom: 20px;
-		}
-		#main-container   .col-md-4 {
-                width: 100%;
-                margin-bottom: 20px;
-		}
-		#main-container   .col-sm-4 {
-                width: 100%;
-                margin-bottom: 20px;
-		}
-        #main-container   .col-sm-5 {
-                width: 34%;
-        }
-		#main-container   .col-sm-6 {
-			    width: 65%;
-		}
-		#main-container   .col-xs-12 {
-                width: 100%;
-		}
-        #main-container   .col-sm-offset-1 {
-                margin-left: 0%;
-        }
-		#main-container   .row .left {
-                float:left;
-                width:180px;
-                text-align:right;
-		}
-		#main-container   .row .left span {
-			    margin-right:0.5em
-		}
-        #main-container .side-menu .text-left, .side-menu .text-right {
-				margin-bottom: 12px;
-        }
-        @media all and (max-width: 600px) {
-                #main-container #content { text-align: center; margin-left: 0em; }
-                #main-container #contentBottom { padding-left: 8%; }
-                span#info-icon {display: inline;position: relative;font-size: 1em;top: 5px;left: 3px;float: left;margin-right: 3px;}
-                #main-container  .help-link { width: 66%;text-align: center;}
-        }
-        @media all and (max-width: 500px) and (min-width: 391px) {
-                #main-container #content { text-align: center; margin-left: 0em; }
-                #main-container #contentBottom { padding-left: 15%; }
-                span#info-icon {position: relative;font-size: 0.8em;top: 7px;left: 5px;float: left;margin-right: 7px;}
-                #main-container  .help-link { width: 56%;text-align: center;}
-        }
-
-        @media all and (max-width: 390px) {
-                #main-container #content { text-align: center; margin-left: 0em; }
-                #main-container #contentBottom { padding-left: 30%;}
-                #main-container message-container { width: 218px; }
-                span#info-icon {position: relative;font-size: 0.8em;top: 7px;left: 5px;float: left;margin-right: 7px;}
-                #main-container  .help-link {width: 33%;text-align: center;}
-        }
-
-        @media all and (max-width: 250px) {
-                #main-container #content { text-align: center; margin-left: 0em; }
-                #main-container #contentBottom { padding-left: 5%; }
-                span#info-icon {position: relative;font-size: 0.8em;top: 7px;left: 5px;float: left;margin-right: 7px;}
-                #main-container  .help-link { width: 29%;text-align: center;}
-        }
+UPDATE CustomComponent SET value = '<style>
+	#main-container {
+		font-family: FrutigerforUBSWeb;
+		max-width: 600px;
+		max-height: 600px;
+		margin-left: auto;
+		margin-right: auto;
+	}
+	#main-container   #pageHeader {
+		width: 100%;
+		height: 74px;
+	}
+	#main-container   #pageHeaderLeft {
+		width: 20%;
+		float: left;
+		padding-left: 16px;
+		padding-top: 16px;
+	}
+	#main-container   #pageHeaderRight {
+		width: 25%;
+		float: right;
+		text-align: right;
+		padding-top: 16px;
+	}
+	#main-container  #issuerLogo {
+		max-height: 64px;
+		max-width:100%;
+		padding-left: 0px;
+		padding-right: 0px;
+	}
+	#main-container   #networkLogo {
+		max-height: 100%;
+		max-width: 100%;
+	}
+	#main-container  #content {
+		text-align:left;
+		margin-left: 3em;
+	}
+	div#leftMenuLayout {
+		font-size: 16px;
+		letter-spacing: 0;
+		line-height: 24px;
+		margin-top: 10px;
+		padding-right: 0px;
+		padding-left: 0px;
+	}
+	div#leftMenuLayout span.ng-binding {
+		font-family: FrutigerforUBSWeb;
+		font-weight: normal;
+		font-size: 16px;
+		color: #1C1C1C;
+		letter-spacing: 0;
+		line-height: 24px;
+	}
+	div#leftMenuLayout span.custom-text.ng-binding {
+		font-family: Frutiger55Roman;
+		font-weight: normal;
+		font-size: 16px;
+		color: #646464;
+		letter-spacing: 0;
+		line-height: 24px;
+	}
+	#main-container .menu-title {
+		display:none;
+	}
+	#main-container .menu-elements {
+		padding-right:10%;
+	}
+	#main-container .side-menu .menu-elements {
+		line-height: 12px;
+	}
+	#main-container #contentBottom {
+		padding-left: 32%;
+	}
+	#message-container {
+		display: inline-block;
+		margin-top: 5px;
+		position: relative;
+		width: 196px;
+	}
+	div#message-content {
+		text-align: center;
+		background-color: inherit;
+		padding-bottom: 5px;
+		background-color:#F7E1DF;
+		font-family: FrutigerforUBSWeb;
+		font-size:12px;
+		color: #E0B519;
+	}
+	span#info-icon {
+		font-size: 1em;
+		top: 5px;
+		left: 5px;
+		float: left;
+		margin-right: 5px;
+	}
+	#message {
+		font-family: FrutigerforUBSWeb;
+		color: #1C1C1C;
+		font-size:12px;
+		line-height: 16px;
+		text-align:center;
+	}
+	span#message {
+		font-size:10px;
+		width: 100%;
+	}
+	div.message-button {
+		padding-top: 0px;
+		display: none;
+	}
+	div#spinner-row .spinner div div{
+		background: #E0B519!important;
+	}
+	#main-container   div#footer {
+		background-image:none;
+		height:44px;
+	}
+	#main-container   #footer {
+		width:100%;
+		clear:both;
+	}
+	#main-container   #footer:after {
+		content: "";
+		height:100%;
+		display: table;
+		clear: both;
+		padding-bottom: 0.5em;
+	}
+	#main-container  #footer .help-area {
+		display: flex;
+		flex-direction: row;
+		padding-top:24px;
+	}
+	#main-container  .help-link {
+		font-size: 14px;
+		line-height: 20px;
+		width: 50%;
+		order: 2;
+		text-align: left;
+		color:#007099;
+	}
+	#main-container #helpButton  button span{
+		color:#007099;
+		background-color: #f7f7f7;
+	}
+	#main-container #helpButton  button {
+		border-style: none;
+		padding:0px
+	}
+	#main-container   #helpButton span.fa-info{
+		display:none;
+	}
+	#main-container   .col-lg-4 {
+		width: 100%;
+		margin-bottom: 20px;
+	}
+	#main-container   .col-md-4 {
+		width: 100%;
+		margin-bottom: 20px;
+	}
+	#main-container   .col-sm-4 {
+		width: 100%;
+		margin-bottom: 20px;
+	}
+	#main-container   .col-sm-5 {
+		width: 34%;
+	}
+	#main-container   .col-sm-6 {
+		width: 65%;
+	}
+	#main-container   .col-xs-12 {
+		width: 100%;
+	}
+	#main-container   .col-sm-offset-1 {
+		margin-left: 0%;
+	}
+	#main-container   .row .left {
+		float:left;
+		width:180px;
+		text-align:right;
+	}
+	#main-container   .row .left span {
+		margin-right:0.5em
+	}
+	#main-container .side-menu .text-left, .side-menu .text-right {
+		margin-bottom: 12px;
+	}
+	@media all and (max-width: 600px) {
+		#main-container #content { text-align: center; margin-left: 0em; }
+		#main-container #contentBottom { padding-left: 8%; }
+		span#info-icon {display: inline;position: relative;font-size: 1em;top: 5px;left: 3px;float: left;margin-right: 3px;}
+		#main-container  .help-link { width: 66%;text-align: center;}
+	}
+	@media all and (max-width: 500px) and (min-width: 391px) {
+		#main-container #content { text-align: center; margin-left: 0em; }
+		#main-container #contentBottom { padding-left: 15%; }
+		span#info-icon {position: relative;font-size: 0.8em;top: 7px;left: 5px;float: left;margin-right: 7px;}
+		#main-container  .help-link { width: 56%;text-align: center;}
+	}
+	@media all and (max-width: 390px) {
+		#main-container #content { text-align: center; margin-left: 0em; }
+		#main-container #contentBottom { padding-left: 30%;}
+		#main-container message-container { width: 218px; }
+		span#info-icon {position: relative;font-size: 0.8em;top: 7px;left: 5px;float: left;margin-right: 7px;}
+		#main-container  .help-link {width: 33%;text-align: center;}
+	}
+	@media all and (max-width: 250px) {
+		#main-container #content { text-align: center; margin-left: 0em; }
+		#main-container #contentBottom { padding-left: 5%; }
+		span#info-icon {position: relative;font-size: 0.8em;top: 7px;left: 5px;float: left;margin-right: 7px;}
+		#main-container  .help-link { width: 29%;text-align: center;}
+	}
 </style>
 <div id="main-container" class="" ng-style="style" class="ng-scope">
-		<div id="headerLayout" class="row">
-			<div id="pageHeader" class="text-center col-xs-12">
-				<div id="pageHeaderLeft" ng-style="style" class="ng-scope">
-					<custom-image alt-key="''network_means_pageType_1_IMAGE_ALT''" image-key="''network_means_pageType_1_IMAGE_DATA''" id="issuerLogo" straight-mode="false"></custom-image>
-				</div>
-				<div id="pageHeaderRight" ng-style="style" class="ng-scope" >
-					<custom-image alt-key="''network_means_pageType_2_IMAGE_ALT''"  image-key="''network_means_pageType_2_IMAGE_DATA''" id="networkLogo" straight-mode="false"></custom-image>
-				</div>
+	<div id="headerLayout" class="row">
+		<div id="pageHeader" class="text-center col-xs-12">
+			<div id="pageHeaderLeft" ng-style="style" class="ng-scope">
+				<custom-image alt-key="''network_means_pageType_1_IMAGE_ALT''" image-key="''network_means_pageType_1_IMAGE_DATA''" id="issuerLogo" straight-mode="false"></custom-image>
 			</div>
-        </div>
-		<div id="content">
-				<div  id="leftMenuLayout" class=" col-xs-12 col-sm-4 col-md-4 col-lg-4">
-					<div>
-						<side-menu menu-title="''TRANSACTION_SUMMARY''"></side-menu>
+			<div id="pageHeaderRight" ng-style="style" class="ng-scope" >
+				<custom-image alt-key="''network_means_pageType_2_IMAGE_ALT''"  image-key="''network_means_pageType_2_IMAGE_DATA''" id="networkLogo" straight-mode="false"></custom-image>
+			</div>
+		</div>
+	</div>
+	<div id="content">
+		<div  id="leftMenuLayout" class=" col-xs-12 col-sm-4 col-md-4 col-lg-4">
+			<div>
+				<side-menu menu-title="''TRANSACTION_SUMMARY''"></side-menu>
+			</div>
+		</div>
+		<div id="contentBottom" class=" col-xs-12 col-sm-4 col-md-4 col-lg-4">
+			<message-banner display-type="''1''" heading-attr="''network_means_pageType_22''" message-attr="''network_means_pageType_23''" close-button="''network_means_pageType_174''" back-button="''network_means_pageType_175''" show=true ></message-banner>
+
+			<div id="footer">
+				<div class="help-area">
+					<div class="help-link">
+						<span class="fa fa-angle-right"></span> <span id="helpButton" class="helpButtonClass"> <custom-text custom-text-key="''network_means_pageType_41''"></custom-text> </span>
 					</div>
 				</div>
-				<div id="contentBottom" class=" col-xs-12 col-sm-4 col-md-4 col-lg-4">
-                    <div id="message-container" ng-class="[style, {unfold: unfolded}]" ng-click="foldUnfold()" click-outside="fold()" class="ng-scope error unfold" style="">
-                        <div id="message-content">
-                            <span id="info-icon" class="fa fa-exclamation-triangle"></span>
-                            <custom-text id="message" custom-text-key="''network_means_pageType_23''"></custom-text>
-                        </div>
-                    </div>
-                    <div id="footer">
-                        <div class="help-area">
-                            <div class="help-link">
-                              <span class="fa fa-angle-right"></span> <span id="helpButton" class="helpButtonClass"> <custom-text custom-text-key="''network_means_pageType_41''"></custom-text> </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+			</div>
 		</div>
+	</div>
 </div>
 ' WHERE `fk_id_layout` = @layoutId;
