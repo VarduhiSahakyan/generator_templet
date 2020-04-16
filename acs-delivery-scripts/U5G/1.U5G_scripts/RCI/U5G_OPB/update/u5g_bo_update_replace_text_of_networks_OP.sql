633332cf3a8016a0211c90ee084edceb918c2dd2
norayrt <8"eX4c[QK3f\8Ahd>
2020-04-16 18:54:51 +0400

    [EWLACS3OP-342] network texts changes. Nitin request to change

--- acs-delivery-scripts/U5G/1.U5G_scripts/RCI/U5G_OPB/update/u5g_bo_update_replace_text_of_networks_OP.sql
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI/U5G_OPB/update/u5g_bo_update_replace_text_of_networks_OP.sql
@@ -1,11 +1,14 @@
 use `U5G_ACS_BO`;
 
 start transaction;
-
+set @masterOld ='Mastercard SecureCode';
+set @masterNew ='Mastercard Identity Check';
+set @visaOld ='Verified by Visa';
+set @visaNew ='VisaSecure';
 update CustomItem
-set value = replace(replace(value, 'Mastercard SecureCode', 'Mastercard Identity Check'), 'Verified by Visa', 'VisaSecure')
+set value = replace(replace(value, @masterOld, @masterNew), @visaOld, @visaNew)
 where fk_id_customItemSet in (select id from CustomItemSet where name like 'customitemset_20000_%')
-  and (value like '%Verified by Visa%' or value like '%Mastercard SecureCode%')
+  and (value like CONCAT('%',@visaOld,'%') or value like CONCAT('%',@masterOld,'%'))
   and pageTypes = 'HELP_PAGE'
   and DTYPE = 'T';
 
