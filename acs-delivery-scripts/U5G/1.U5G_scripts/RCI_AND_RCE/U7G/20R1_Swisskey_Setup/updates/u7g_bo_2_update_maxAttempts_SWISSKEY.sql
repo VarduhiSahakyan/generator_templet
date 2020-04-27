b346077a25f7708750f22c9e2990fbb93766cc33
Emma Abgaryan <emmaa@tce.loc>
2020-04-27 19:01:22 +0400

    [EWLITAGEPG-859] clear code fixes

--- acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U7G/20R1_Swisskey_Setup/updates/u7g_bo_2_update_maxAttempts_SWISSKEY.sql
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U7G/20R1_Swisskey_Setup/updates/u7g_bo_2_update_maxAttempts_SWISSKEY.sql
@@ -6,7 +6,12 @@ SET @BankSGKB = 'SGKB';
 
 
 Update `Profile` SET `maxAttempts` = 4
-WHERE `name` = CONCAT(@BankCS,'_TA_01') or `name` = CONCAT(@BankNAB,'_TA_01') or `name` = CONCAT(@BankSGKB,'_TA_01') or`name` = CONCAT(@BankCS,'_SMS_01') or`name` = CONCAT(@BankNAB,'_SMS_01') or`name` = CONCAT(@BankSGKB,'_SMS_01');
+WHERE `name` = CONCAT(@BankCS,'_TA_01') 
+   or `name` = CONCAT(@BankNAB,'_TA_01') 
+   or `name` = CONCAT(@BankSGKB,'_TA_01') 
+   or`name` = CONCAT(@BankCS,'_SMS_01') 
+   or`name` = CONCAT(@BankNAB,'_SMS_01') 
+   or`name` = CONCAT(@BankSGKB,'_SMS_01');
 
 
 
