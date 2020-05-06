5174a7e42b4faed062f96ba2bfa5577b9a3b3489
Emma Abgaryan <emmaa@tce.loc>
2020-05-06 12:05:23 +0400

    [EWLACS3CDT-122] Changed issuer logo sizes

--- acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/20R1_AppBasedFlow/HTML_APP_Based/ComDirect_3DS2_HTMLView/01_u5g_acs_bo_Comdirect_OTP_SMS_App.sql
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/20R1_AppBasedFlow/HTML_APP_Based/ComDirect_3DS2_HTMLView/01_u5g_acs_bo_Comdirect_OTP_SMS_App.sql
@@ -24,6 +24,13 @@ values ('div',
 	.acs-header {
 		margin-bottom: 0.5em;
 	}
+    #issuerLogo{
+        width: 120px;
+        height: 33px;
+    }
+    #networkLogo {
+        padding-left: 50px;
+    }
 	.acs-purchase-context {
 		margin-bottom: 2em;
 		margin-top: 0.5em;
@@ -170,10 +177,10 @@ values ('div',
 	<!-- ACS HEADER | Branding zone-->
 	<div class="acs-header row branding-zone">
 		<div class="col-md-6">
-			<img src="network_means_pageType_251" alt="Issuer image" data-cy="ISSUER_IMAGE"/>
+			<img src="network_means_pageType_251" id = "issuerLogo" alt="Issuer image" data-cy="ISSUER_IMAGE"/>
 		</div>
 		<div class="col-md-6">
-			<img src="network_means_pageType_254" alt="Card network image" data-cy="CARD_NETWORK_IMAGE"/>
+			<img src="network_means_pageType_254" id = "networkLogo" alt="Card network image" data-cy="CARD_NETWORK_IMAGE"/>
 		</div>
 	</div>
 	<!-- ACS BODY | Challenge/Processing zone -->
