cfa8bd0298801a48ad5966fa2d1f15fdbc8cc1e8
Bruno Gallet <bruno.gallet@atos.net>
2020-06-17 17:12:16 +0200

    [U5G][00070-CBC]EWLACSCHN-149 correct table name

--- acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/03.00070_CBC/EWLACSCHN-149.sql
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/03.00070_CBC/EWLACSCHN-149.sql
@@ -1,3 +1,3 @@
 USE `U5G_ACS_BO`;
 
-UPDATE `Subissuer` SET `dateFormat` = 'lll|Asia/Singapore' WHERE `code` = '00070';
+UPDATE `SubIssuer` SET `dateFormat` = 'lll|Asia/Singapore' WHERE `code` = '00070';
