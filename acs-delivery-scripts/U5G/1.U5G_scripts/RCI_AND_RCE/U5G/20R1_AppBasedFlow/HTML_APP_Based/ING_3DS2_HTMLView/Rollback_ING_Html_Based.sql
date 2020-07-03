3b5d4855be6a708188b79482b22e621faff4699d
Sneh Brat <sneh.brat@atos.net>
2020-07-03 16:22:00 +0530

    ING device choice page Script for HTML App based.

--- acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/20R1_AppBasedFlow/HTML_APP_Based/ING_3DS2_HTMLView/Rollback_ING_Html_Based.sql
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/20R1_AppBasedFlow/HTML_APP_Based/ING_3DS2_HTMLView/Rollback_ING_Html_Based.sql
@@ -7,15 +7,17 @@ set foreign_key_checks = 0;
 SET @idAppViewPagePassword=(SELECT id FROM `CustomPageLayout` WHERE `pageType`= 'EXT_PASSWORD_APP_VIEW' and DESCRIPTION = 'EXT_PASSWORD_APP_VIEW (ING)');
 SET @idAppViewPageSMS=(SELECT id FROM `CustomPageLayout` WHERE `pageType`= 'OTP_SMS_EXT_MESSAGE_APP_VIEW' and DESCRIPTION = 'SMS_App_View (ING)') ;
 SET @idAppViewPageTA=(SELECT id FROM `CustomPageLayout` WHERE `pageType`= 'MOBILE_APP_EXT_APP_VIEW' and DESCRIPTION = 'MOBILE_APP_EXT_App_View (ING)') ;
+SET @idAppViewPageDeviceChoiceTA=(SELECT id FROM `CustomPageLayout` WHERE `pageType`= 'MOBILE_APP_EXT_APP_VIEW_DEVICE_SELECT' and DESCRIPTION = 'DEVICE_Choice_App_View (ING)') ;
 SET @profileSetId = (SELECT id FROM `ProfileSet` WHERE `name` ='PS_16500_01');
 
 
-delete from CustomPageLayout_ProfileSet where customPageLayout_id in(@idAppViewPageSMS,@idAppViewPageTA,@idAppViewPagePassword) and profileSet_id =@profileSetId;
-delete from CustomComponent where fk_id_layout in (@idAppViewPageSMS,@idAppViewPageTA,@idAppViewPagePassword);
+delete from CustomPageLayout_ProfileSet where customPageLayout_id in(@idAppViewPageSMS,@idAppViewPageTA,@idAppViewPagePassword,@idAppViewPageDeviceChoiceTA) and profileSet_id =@profileSetId;
+delete from CustomComponent where fk_id_layout in (@idAppViewPageSMS,@idAppViewPageTA,@idAppViewPagePassword,@idAppViewPageDeviceChoiceTA);
 
 delete from CustomPageLayout where pageType like '%OTP_SMS_EXT_MESSAGE_APP_VIEW%' and description like '%SMS_App_View (ING)%';
 delete from CustomPageLayout where pageType like '%EXT_PASSWORD_APP_VIEW%' and description like '%EXT_PASSWORD_APP_VIEW (ING)%';
 delete from CustomPageLayout where pageType like '%MOBILE_APP_EXT_APP_VIEW%' and description like '%MOBILE_APP_EXT_App_View (ING)%';
+delete from CustomPageLayout where pageType like '%MOBILE_APP_EXT_APP_VIEW_DEVICE_SELECT%' and description like '%DEVICE_Choice_App_View (ING)%';
 
 
 set foreign_key_checks = 1; 
