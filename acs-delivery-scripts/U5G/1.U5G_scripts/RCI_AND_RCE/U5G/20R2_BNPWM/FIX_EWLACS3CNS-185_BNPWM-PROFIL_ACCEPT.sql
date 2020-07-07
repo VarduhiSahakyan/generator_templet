822b027e52b7a656ab4f79de22360bf387a0aae2
jld <jean-luc.dugardin@atos.net>
2020-07-07 12:45:19 +0200

    REF #EWLACS3CNS-185 FIXBNPWM-PROFIL ACCEPT

--- acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/20R2_BNPWM/FIX_EWLACS3CNS-185_BNPWM-PROFIL_ACCEPT.sql
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/20R2_BNPWM/FIX_EWLACS3CNS-185_BNPWM-PROFIL_ACCEPT.sql
@@ -1,7 +1,5 @@
 SELECT id INTO @id_ACCEPT FROM AuthentMeans WHERE NAME = 'ACCEPT';
-SELECT 3 INTO @MAX_ATTEMPTS;
 
 UPDATE Profile
-SET fk_id_AuthentMeans=@id_ACCEPT,
-maxAttempts = @MAX_ATTEMPTS
+SET fk_id_AuthentMeans=@id_ACCEPT
 WHERE NAME = 'BNP_WM_ACCEPT_01';
