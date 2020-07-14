f02a75d98b763508dee9beaa18646c11308cbdb3
Vahe Hovsepyan <vaheh@tce.loc>
2020-07-14 18:44:40 +0400

    moved CW28 production scripts

---
+++ acs-delivery-scripts/U5G/1.U5G_scripts/PRD/fixes/U7G/U7G-SWK/20R1/CW28/04_EWLITAGEPG-811.sql
@@ -0,0 +1,13 @@
+
+USE `U7G_ACS_BO`;
+
+SET @subIssuerCS = 'Crédit Suisse AG';
+SET @subIssuerNAB = 'Neue Aargauer Bank';
+SET @subIssuerSOBA = 'Baloise Bank SoBa AG';
+SET @subIssuerLUKB = 'Luzerner KB AG';
+
+UPDATE `SubIssuer` SET `userChoiceAllowed` = false
+				   WHERE `name` = @subIssuerCS
+					  OR `name` = @subIssuerNAB
+					  OR `name` = @subIssuerSOBA
+					  OR `name` = @subIssuerLUKB;
--- acs-delivery-scripts/U5G/1.U5G_scripts/PRD/fixes/U7G/U7G-UBS/20R1/CW28/03_EWLACS3SWK-269.sql
+++ acs-delivery-scripts/U5G/1.U5G_scripts/PRD/fixes/U7G/U7G-UBS/20R1/CW28/03_EWLACS3SWK-269.sql
@@ -36,4 +36,14 @@ where fk_id_customItemSet in (select id
   and name like '%VISA_REFUSAL_ALL_174'
   and DTYPE = 'T';
 
+update CustomItem
+set fk_id_network = null
+where fk_id_customItemSet in (select id
+							  from CustomItemSet
+							  where name in ('customitemset_UBS_SMS', 'customitemset_UBS_MOBILE_APP_EXT',
+											 'customitemset_UBS_DEFAULT_REFUSAL',
+											 'customitemset_UBS_MISSING_AUTHENTICATION_REFUSAL'))
+  and ordinal = 174
+  and DTYPE = 'T';
+
 commit;
