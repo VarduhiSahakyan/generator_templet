9a5a1faf37f7f210dd3e473e13a2cd541c44e3c1
Gevorg Aghajanyan <gevorga@energizeglobal.com>
2020-07-30 17:16:34 +0400

    [EWLACS3RBK-101] Successful Banner - Typos in Text

---
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/ReiseBank/04_EWLACS3RBK-101.sql
@@ -0,0 +1,21 @@
+USE U5G_ACS_BO;
+
+SET @subIssuerCode = 12000;
+
+SET @subIssuerId = (SELECT id FROM SubIssuer WHERE code = @subIssuerCode);
+
+SET @customItemSetName = 'customitemset_12000_REISEBANK_SMS_01';
+
+SET @customItemSetId = (SELECT id FROM CustomItemSet where fk_id_subIssuer = @subIssuerId AND name = @customItemSetName);
+
+UPDATE CustomItem SET value = 'Authentication successful'
+    WHERE fk_id_customItemSet = @customItemSetId
+      AND ordinal = 26
+      AND name = 'OTP_FORM_PAGE'
+      AND locale = 'en';
+
+UPDATE CustomItem SET value = 'Your authentication has been validated, you will be redirected to the merchant website.'
+    WHERE fk_id_customItemSet = @customItemSetId
+      AND ordinal = 27
+      AND name = 'OTP_FORM_PAGE'
+      AND locale = 'en';
