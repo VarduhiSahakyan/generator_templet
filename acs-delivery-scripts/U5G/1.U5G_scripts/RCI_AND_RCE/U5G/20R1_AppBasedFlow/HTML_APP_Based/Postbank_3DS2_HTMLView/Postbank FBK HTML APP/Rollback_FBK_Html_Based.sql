6b1028493bdf82bbe4918b05f7d9b4deed57390b
Sneh Brat <sneh.brat@atos.net>
2020-08-04 17:33:11 +0530

    Postbank Choice page implementation and white space change added.

--- acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/20R1_AppBasedFlow/HTML_APP_Based/Postbank_3DS2_HTMLView/Postbank FBK HTML APP/Rollback_FBK_Html_Based.sql
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/20R1_AppBasedFlow/HTML_APP_Based/Postbank_3DS2_HTMLView/Postbank FBK HTML APP/Rollback_FBK_Html_Based.sql
@@ -7,7 +7,7 @@ set foreign_key_checks = 0;
 SET @idAppViewPagePassword=(SELECT id FROM `CustomPageLayout` WHERE `pageType`= 'PASSWORD_APP_VIEW' and DESCRIPTION = 'PASSWORD_APP_VIEW (FBK)');
 SET @idAppViewPageSMS=(SELECT id FROM `CustomPageLayout` WHERE `pageType`= 'OTP_SMS_APP_VIEW' and DESCRIPTION = 'SMS_App_View (FBK)') ;
 SET @idAppViewPageTA=(SELECT id FROM `CustomPageLayout` WHERE `pageType`= 'MOBILE_APP_APP_VIEW' and DESCRIPTION = 'MOBILE_APP_APP_VIEW (FBK)') ;
-SET @idAppViewPageCHOICE=(SELECT id FROM `CustomPageLayout` WHERE `pageType`= 'UNDEFINED_APP_VIEW' and DESCRIPTION = 'Choice_App_View (FBK)') ;
+SET @idAppViewPageCHOICE=(SELECT id FROM `CustomPageLayout` WHERE `pageType`= 'UNDEFINED_APP_VIEW_MEAN_SELECT' and DESCRIPTION = 'Choice_App_View (FBK)') ;
 SET @profileSetId = (SELECT id FROM `ProfileSet` WHERE `name` ='PS_18502_PB_01');
 
 
@@ -17,7 +17,7 @@ delete from CustomComponent where fk_id_layout in (@idAppViewPageSMS,@idAppViewP
 delete from CustomPageLayout where pageType like '%OTP_SMS_APP_VIEW%' and description like '%SMS_App_View (FBK)%';
 delete from CustomPageLayout where pageType like '%PASSWORD_APP_VIEW%' and description like '%PASSWORD_APP_VIEW (FBK)%';
 delete from CustomPageLayout where pageType like '%MOBILE_APP_APP_VIEW%' and description like '%MOBILE_APP_APP_VIEW (FBK)%';
-delete from CustomPageLayout where pageType like '%UNDEFINED_APP_VIEW%' and description like '%Choice_App_View (FBK)%';
+delete from CustomPageLayout where pageType like '%UNDEFINED_APP_VIEW_MEAN_SELECT%' and description like '%Choice_App_View (FBK)%';
 
 
 set foreign_key_checks = 1; 
