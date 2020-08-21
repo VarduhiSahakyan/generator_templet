3fdd82c6b0a5e0b31434935825fab55ea6a8bf06
Bruno Gallet <bruno.gallet@atos.net>
2020-08-21 10:32:21 +0200

    Update Sparda

--- acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/SPB_Setup/5.1.add.role.to.user-PORTAL_SERVICE_PARAM-SPB.sql
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/SPB_Setup/5.1.add.role.to.user-PORTAL_SERVICE_PARAM-SPB.sql
@@ -1,8 +1,5 @@
 USE PORTAL_SERVICE_PARAM;
 
-/* Christian Schnur */
-SET @userLogin = 'EWLA790281';
-
 -- check existing 
 SET @serviceName = 'ACS_U5G';
 SET @serviceNamePortail = 'PORTAIL';
@@ -18,8 +15,40 @@ SET @roleId = (SELECT ROLE_ID FROM ROLE WHERE ROLE_NAME = @roleName);
 SET @serviceId = (SELECT SERVICE_ID FROM SERVICE WHERE SERVICE_NAME = @serviceName);
 SET @servicePortailId = (SELECT SERVICE_ID FROM SERVICE WHERE SERVICE_NAME = @serviceNamePortail);
 
+/* Christian Schnur */
+SET @userLogin = 'EWLA790281';
 INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @serviceId);
 INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @servicePortailId);
+
+-- Nicolas Pascal
+SET @userLogin = 'A176981';
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @serviceId);
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @servicePortailId);
+-- Aarti Priya
+SET @userLogin = 'EWLA185629';
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @serviceId);
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @servicePortailId);
+
+-- Dajana Eisenhauer
+SET @userLogin = 'EWLdEisenhauer';
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @serviceId);
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @servicePortailId);
+
+-- Tillmann Schmitt
+SET @userLogin = 'A562490';
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @serviceId);
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @servicePortailId);
+
+-- Nicolas Engel
+SET @userLogin = 'A663859';
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @serviceId);
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @servicePortailId);
+
+-- Tim Milde
+SET @userLogin = 'A758569';
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @serviceId);
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @servicePortailId);
+
 
 -- check existing 
 SELECT * FROM PROFIL p 
@@ -45,6 +74,37 @@ SET @roleId = (SELECT ROLE_ID FROM ROLE WHERE ROLE_NAME = @roleName);
 SET @serviceId = (SELECT SERVICE_ID FROM SERVICE WHERE SERVICE_NAME = @serviceName);
 SET @servicePortailId = (SELECT SERVICE_ID FROM SERVICE WHERE SERVICE_NAME = @serviceNamePortail);
 
+-- Christian Schnur
+SET @userLogin = 'EWLA790281';
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @serviceId);
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @servicePortailId);
+
+-- Nicolas Pascal
+SET @userLogin = 'A176981';
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @serviceId);
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @servicePortailId);
+-- Aarti Priya
+SET @userLogin = 'EWLA185629';
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @serviceId);
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @servicePortailId);
+
+-- Dajana Eisenhauer
+SET @userLogin = 'EWLdEisenhauer';
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @serviceId);
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @servicePortailId);
+
+-- Tillmann Schmitt
+SET @userLogin = 'A562490';
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @serviceId);
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @servicePortailId);
+
+-- Nicolas Engel
+SET @userLogin = 'A663859';
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @serviceId);
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @servicePortailId);
+
+-- Tim Milde
+SET @userLogin = 'A758569';
 INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @serviceId);
 INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @servicePortailId);
 
@@ -71,9 +131,40 @@ SET @roleId = (SELECT ROLE_ID FROM ROLE WHERE ROLE_NAME = @roleName);
 SET @serviceId = (SELECT SERVICE_ID FROM SERVICE WHERE SERVICE_NAME = @serviceName);
 SET @servicePortailId = (SELECT SERVICE_ID FROM SERVICE WHERE SERVICE_NAME = @serviceNamePortail);
 
+-- Christian Schnur
+SET @userLogin = 'EWLA790281';
 INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @serviceId);
 INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @servicePortailId);
 
+-- Nicolas Pascal
+SET @userLogin = 'A176981';
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @serviceId);
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @servicePortailId);
+
+-- Aarti Priya
+SET @userLogin = 'EWLA185629';
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @serviceId);
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @servicePortailId);
+
+-- Dajana Eisenhauer
+SET @userLogin = 'EWLdEisenhauer';
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @serviceId);
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @servicePortailId);
+
+-- Tillmann Schmitt
+SET @userLogin = 'A562490';
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @serviceId);
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @servicePortailId);
+
+-- Nicolas Engel
+SET @userLogin = 'A663859';
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @serviceId);
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @servicePortailId);
+
+-- Tim Milde
+SET @userLogin = 'A758569';
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @serviceId);
+INSERT INTO PROFIL (USER_XARM_LOGIN, ROLE_ID, SERVICE_ID) VALUES (@userLogin, @roleId, @servicePortailId);
 
 -- check existing 
 SELECT * FROM PROFIL p 
