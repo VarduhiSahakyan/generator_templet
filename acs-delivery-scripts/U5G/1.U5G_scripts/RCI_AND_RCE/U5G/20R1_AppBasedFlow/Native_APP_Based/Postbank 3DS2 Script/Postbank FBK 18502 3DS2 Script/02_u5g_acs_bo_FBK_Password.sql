fdf986a528767ade68484c75f293baa8f7b66a06
Sneh Brat <sneh.brat@atos.net>
2020-08-24 16:59:31 +0530

    EWLACS3PSB-176- Postbank FBK blank space for the Challenge information text.

--- acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/20R1_AppBasedFlow/Native_APP_Based/Postbank 3DS2 Script/Postbank FBK 18502 3DS2 Script/02_u5g_acs_bo_FBK_Password.sql
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/20R1_AppBasedFlow/Native_APP_Based/Postbank 3DS2 Script/Postbank FBK 18502 3DS2 Script/02_u5g_acs_bo_FBK_Password.sql
@@ -66,7 +66,7 @@ INSERT INTO `CustomItem` (`DTYPE`, `createdBy`, `creationDate`, `description`, `
 
 -- 3DS MAIN CONTENT TEXT
 SET @ordinal = 152;
-SET @text = 'Damit Sie noch sicherer mit Ihrer Kreditkarte zahlen, haben wir eine zusätzliche Abfrage eingefügt.Bitte geben Sie Ihr Passwort für den Kreditkarten Online-Service ein. \n\nAbrechnungskonto : @pam \nHändler : @merchant \nBetrag : @amount \nDatum : @formattedDate \nKartennummer : @displayedPan';
+SET @text = 'Damit Sie noch sicherer mit Ihrer Kreditkarte zahlen, haben wir eine zusätzliche Abfrage eingefügt. Bitte geben Sie Ihr Passwort für den Kreditkarten Online-Service ein. \n\nAbrechnungskonto : @pam \nHändler : @merchant \nBetrag : @amount \nDatum : @formattedDate \nKartennummer : @displayedPan';
 INSERT INTO `CustomItem` (`DTYPE`, `createdBy`, `creationDate`, `description`, `lastUpdateBy`, `lastUpdateDate`, `name`, `updateState`, `locale`, `ordinal`, `pageTypes`, `value`, `fk_id_network`, `fk_id_image`, `fk_id_customItemSet`)
   	SELECT 'T', @username, NOW(), NULL, NULL, NULL, CONCAT(n.code,'_',@amName,'_',@pageType,'_',@ordinal,'_',@locale), 'PUSHED_TO_CONFIG', @locale, @ordinal, @pageType, @text, NULL, NULL, @customItemSetId FROM `Network` n WHERE  n.id in (@networkVISA, @networkMC);
 
