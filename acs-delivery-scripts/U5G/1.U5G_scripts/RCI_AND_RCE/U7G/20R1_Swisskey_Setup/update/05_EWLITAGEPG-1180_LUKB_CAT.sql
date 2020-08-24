4c682ed75198c93269b0143f55582cb6f36b4b88
Emma Abgaryan <emmaa@tce.loc>
2020-08-24 17:24:46 +0400

    [EWLITAGEPG-1180] Fixed Visa Logo for LUKB CAT

---
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U7G/20R1_Swisskey_Setup/update/05_EWLITAGEPG-1180_LUKB_CAT.sql
@@ -0,0 +1,14 @@
+USE `U7G_ACS_BO`;
+
+SET @createdBy ='A758582';
+SET @subIssuerCode = '77800';
+SET @BankUB = 'LUKB';
+
+SET @customItemSetSMS = (SELECT id FROM `CustomItemSet` WHERE `name` = CONCAT('customitemset_', @BankUB, '_SMS'));
+SET @customItemSetMobileApp = (SELECT id FROM `CustomItemSet` WHERE `name` = CONCAT('customitemset_', @BankUB, '_MOBILE_APP'));
+SET @visaLogoId = (SELECT id from `Image` WHERE `name` = 'VISA_LOGO');
+
+UPDATE `CustomItem` SET `fk_id_image` = @visaLogoId
+					WHERE `DTYPE` = 'I'
+					AND `value` = 'Verified by Visa™'
+					AND `fk_id_customItemSet` IN (@customItemSetSMS, @customItemSetMobileApp);
