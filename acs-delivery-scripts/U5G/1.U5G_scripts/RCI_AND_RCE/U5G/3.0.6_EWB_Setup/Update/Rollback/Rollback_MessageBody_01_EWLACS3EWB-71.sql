48f2a1b7a125df4ee72788a444a66c07447578c5
Saichin Kuttipurath <saichin.kuttipurath@atos.net>
2020-08-28 13:01:56 +0530

    EWLACS3EWB-71 :: Changes for Message Body Update Script

---
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/3.0.6_EWB_Setup/Update/Rollback/Rollback_MessageBody_01_EWLACS3EWB-71.sql
@@ -0,0 +1,8 @@
+USE `U5G_ACS_BO`;
+
+SET @EWB_SMS_CustomItemSet  = 'customitemset_EWB_SMS';
+SET @EWB_SMS_CustomItemSet_Id  = (SELECT id FROM `U5G_ACS_BO`.`CustomItemSet` WHERE `name` =@EWB_SMS_CustomItemSet);
+
+SET @pageTypes  = 'MESSAGE_BODY';
+
+UPDATE `U5G_ACS_BO`.`CustomItem` SET `value`='EWB: You have a pending transaction at @merchant for @amount. DO NOT SHARE. If valid, pls use OTP @otp. Call 88881700 for concerns.' WHERE  fk_id_customItemSet IN (@EWB_SMS_CustomItemSet_Id) AND pageTypes = @pageTypes;
