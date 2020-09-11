763ac74518108882a4c9acf20bfa75c7566f572c
Emma Abgaryan <emmaa@tce.loc>
2020-09-11 16:28:35 +0400

    [EWLACS3SWK-40] Changed currencyFormat in Rollback for UBS

--- acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U7G/common_updates/Rollback_EWLACS3SWK-40.sql
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U7G/common_updates/Rollback_EWLACS3SWK-40.sql
@@ -1,7 +1,14 @@
 USE U7G_ACS_BO;
 
 SET @currencyFormat = null;
-
 SET @issuerId_SWISSKEY = (SELECT `id` FROM `Issuer` WHERE `code` = 41001);
+
+UPDATE `SubIssuer` SET `currencyFormat` = @currencyFormat WHERE `fk_id_issuer` = @issuerId_SWISSKEY;
+
+
+SET @currencyFormat = '{
+							"useAlphaCurrencySymbol":true,
+							"currencySymbolPosition":"LEFT"
+						}';
 SET @issuerId_UBS = (SELECT `id` FROM `Issuer` WHERE `code` = 23000);
-UPDATE `SubIssuer` SET `currencyFormat` = @currencyFormat WHERE `fk_id_issuer` IN (@issuerId_SWISSKEY, @issuerId_UBS);
+UPDATE `SubIssuer` SET `currencyFormat` = @currencyFormat WHERE `fk_id_issuer` = @issuerId_UBS;
