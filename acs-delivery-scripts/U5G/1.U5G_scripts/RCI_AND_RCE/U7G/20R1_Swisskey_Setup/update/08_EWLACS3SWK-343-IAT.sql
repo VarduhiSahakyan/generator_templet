1da69ea970394b7c7071c8aa6ec3b6eff64392d2
Vahe Hovsepyan <vaheh@tce.loc>
2020-09-30 17:07:15 +0400

    [U7G] moved production scripts for CW37

--- acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U7G/20R1_Swisskey_Setup/update/08_EWLACS3SWK-343-IAT.sql
+++
@@ -1,8 +0,0 @@
-USE U7G_ACS_BO;
-
-SET @customItemSetLLBPassword = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_LLB_PASSWORD');
-
-UPDATE `CustomItem` ci SET ci.`value` = replace(ci.value,  '@maskedPan', '@displayedPan')
-WHERE `fk_id_customItemSet` in (@customItemSetLLBPassword)
-  AND `ordinal` = 152
-  AND `pageTypes` = 'APP_VIEW';
