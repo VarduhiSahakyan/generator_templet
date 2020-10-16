c8d2a457ff2ea2653b9cc4e212eeaa4fe6066757
Sneh Brat <sneh.brat@atos.net>
2020-10-16 14:28:54 +0530

    EWLACS3SPD-58 changes.

---
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/20R1_AppBasedFlow/Native_APP_Based/Sparda_3DS2_AppView/Update/01_EWLACS3SPD-58.sql
@@ -0,0 +1,6 @@
+USE `U5G_ACS_BO`;
+
+
+SET @customItemSetId = (SELECT id FROM CustomItemSet WHERE name = 'customitemset_SPB_sharedBIN_SMS_APP');
+
+UPDATE `CustomItem` SET VALUE = 'Schritt 2: Auswahl TAN-Verfahren' WHERE fk_id_customItemSet in (@customItemSetId) and ordinal = 151 and pageTypes = 'APP_VIEW_MEAN_SELECT' and DTYPE ='T';
