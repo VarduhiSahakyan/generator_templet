a1eb39bfc4b7edeef6f6fef006f08c4d9a366842
Vahe Hovsepyan <vaheh@tce.loc>
2020-11-26 14:11:37 +0400

    [U7G] moved CW45 scripts into PROD directory

--- acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U7G/20R1_Swisskey_Setup/llb/updates/03_EWLACS3SWK-398_LLB_IAT_CAT.sql
+++
@@ -1,9 +0,0 @@
-USE U7G_ACS_BO;
-
-SET @BankB = 'SWISSKEY';
-SET @BankUB = 'LLB';
-
-UPDATE `CustomPageLayout` SET `pageType` = 'PASSWORD_APP_VIEW',
-                              `description` = 'App_View PASSWORD (SWISSKEY)'
-                        where `pageType` = 'EXT_PASSWORD_APP_VIEW' AND
-                              `description` = 'App_View EXT_PASSWORD (SWISSKEY)' ;
