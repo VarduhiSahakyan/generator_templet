201196cb0e506205665393936300e1482f7d113a
Vahe Hovsepyan <vaheh@tce.loc>
2021-01-12 14:24:29 +0400

    [LBBW][EWLITAGEPG-1464] fixed help text issue

--- acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/LBBW/update/EWLITAGEPG-1464.sql
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/LBBW/update/EWLITAGEPG-1464.sql
@@ -5,10 +5,7 @@ SET @username = 'W100851';
 SET @customItemSet_LBBW_OTP_SMS = (SELECT id FROM CustomItemSet WHERE name = 'customitemset_LBBW_OTP_SMS');
 SET @pageType_HELP_PAGE ='HELP_PAGE';
 
-SET @text = 'Ihre Kreditkartenzahlung muss aus Sicherheitsgründen mit 3D-Secure bestätigt werden.
-Dazu erhalten Sie eine mTAN per SMS auf Ihr Mobiltelefon.
-Das SMS mTAN Verfahren wird zum 31.05.2021 von uns deaktiviert. Bitte registrieren Sie sich für das neue Sicherheitsverfahren BW-Secure. Diese ist in wenigen Minuten schnell und einfach erledigt. Gleich anmelden im BW-Secure Portal:<br>
-<a href="https://sicheres-bezahlen.bw-bank.de/" target="_blank">https://sicheres-bezahlen.bw-bank.de/</a>';
+SET @text = 'Das SMS mTAN Verfahren wird zum 31.05.2021 von uns deaktiviert. Bitte registrieren Sie sich für das neue Sicherheitsverfahren BW-Secure. Diese ist in wenigen Minuten schnell und einfach erledigt. Gleich anmelden im BW-Secure Portal:<br><a href="https://sicheres-bezahlen.bw-bank.de/" target="_blank">https://sicheres-bezahlen.bw-bank.de/</a>';
 
 UPDATE `CustomItem` SET `value` = @text
 					WHERE `locale` = @locale
