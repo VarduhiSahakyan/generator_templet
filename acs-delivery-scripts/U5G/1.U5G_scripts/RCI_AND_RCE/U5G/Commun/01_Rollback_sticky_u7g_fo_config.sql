4cc6e081dc9c7ca0b771e2ece3e25c98b0a4dd3a
Bruno Gallet <bruno.gallet@atos.net>
2021-01-19 19:05:59 +0100

    [U7G][EWLITAGEPG-1059] paChallengePublicUrl / Sticky Session

---
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/Commun/01_Rollback_sticky_u7g_fo_config.sql
@@ -0,0 +1,6 @@
+
+USE `U7G_ACS_FO`;
+
+
+UPDATE `ISSUER_CONFIG`
+SET `PA_CHALLENGE_PUBLIC_URL` = '{ "Vendome" : "https://secure.six-group.com/", "Brussels" : "https://secure.six-group.com/", "Unknown" : "https://secure.six-group.com/" }';
