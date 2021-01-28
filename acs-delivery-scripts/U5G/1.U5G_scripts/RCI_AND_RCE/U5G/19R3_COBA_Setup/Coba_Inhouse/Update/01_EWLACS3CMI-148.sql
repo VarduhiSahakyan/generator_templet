fd32b017e61b06ddb4fe200cb64337d28e33294e
Saichin Kuttipurath <saichin.kuttipurath@atos.net>
2021-01-28 16:55:47 +0530

    CMI-148 :: Adding changes for Alternate Display

---
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/19R3_COBA_Setup/Coba_Inhouse/Update/01_EWLACS3CMI-148.sql
@@ -0,0 +1,260 @@
+USE `U5G_ACS_BO`;
+
+SET @networkVISA = (SELECT id FROM `Network` WHERE `code` = 'VISA');
+SET @networkMC = (SELECT id FROM `Network` WHERE `code` = 'MASTERCARD');
+
+SET @createdBy ='A708534';
+SET @locale = 'de';
+SET @updateState = 'PUSHED_TO_CONFIG';
+
+SET @authentMean ='_REFUSAL_';
+
+SET @pageTypes = 'REFUSAL_PAGE';
+SET @customItemSetRefusal = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_COZ_1_REFUSAL');
+
+SET @ordinalMessage = 201;
+SET @ordinalRefusalMessage = 23;
+
+
+  
+INSERT INTO `CustomItem` (`DTYPE`, `createdBy`, `creationDate`, `description`, `lastUpdateBy`, `lastUpdateDate`,
+                          `name`, `updateState`, `locale`, `ordinal`, `pageTypes`, `value`,
+                          `fk_id_network`, `fk_id_image`, `fk_id_customItemSet`)
+SELECT 'T', @createdBy, NOW(), NULL, NULL, NULL, CONCAT(n.code,@authentMean,@pageTypes,'_',@ordinalMessage), @updateState, @locale, @ordinalMessage,  @pageTypes, 'Bitte registrieren Sie Ihre Kreditkarte für das sichere Einkaufen unter commerzbank.de/sicher-einkaufen', n.id, NULL, @customItemSetRefusal FROM `Network` n WHERE  n.id in (@networkVISA, @networkMC);
+
+
+UPDATE `CustomItem` SET `value`='Bitte aktivieren Sie Ihre Kreditkarte im Online Banking oder wenden Sie sich an den Ansprechpartner in Ihrer Filiale.' WHERE  fk_id_customItemSet IN (@customItemSetRefusal) AND `ordinal` = @ordinalRefusalMessage;
+
+
+
+SET @customPageLayoutRefusal = (SELECT id FROM `CustomPageLayout` WHERE `description` = 'Refusal Page (Commerzbank AG)');
+
+UPDATE `CustomComponent` SET `value` = '
+
+<style>
+	div#optGblPage {
+		font-family: Arial,bold;
+		color: #333333;
+		font-size:14px;
+		overflow:auto;
+	}
+	#footer #cancelButton button span:before {
+		content:'''';
+	}
+	#footer {
+		padding-top: 12px;
+		padding-bottom:12px;
+		clear:both; 
+		width:100%;
+		background-color:#FFFFFF;
+		text-align:center;
+		margin-top: 15px;
+		margin-bottom : 15px;
+	}
+	#issuerLogo {
+		max-height: 64px;
+		max-width:100%;
+		padding-left: 0px;
+		padding-right: 0px;
+	}
+	#networkLogo {
+		max-height: 80px;
+		max-width:100%;
+		padding-right: 16px;
+		padding-bottom: 10px;
+	}
+	#pageHeader {
+		width: 100%;
+		height: 95px;
+		border-bottom: 1px solid #DCDCDC;
+	}
+	#pageHeaderLeft {
+		width: 20%;
+		float: left;
+		padding-left: 16px;
+		padding-top: 16px;
+	}
+	#pageHeaderCenter {
+		width: 60%;
+		float: left;
+		text-align: center;
+		line-height: 70px;
+		padding-top: 16px;
+	}
+	#pageHeaderRight {
+		width: 80%;
+		float: right;
+		text-align: right;
+		padding-left: 16px;
+	}
+	.leftColumn {
+		width:65%;
+		display:block;
+		float:left;
+		padding:1em;
+	}
+	.rightColumn {
+		width: 100%;
+		margin-top: 10%;
+		display: block;
+		text-align: center;
+		padding: 10px 62px 10px;
+		border: 1px solid; 
+		background-color: #EAEAEA; 
+		border-color: #C1C1C1;
+	}
+	.contentRow {
+		width:100%;
+		padding:1em;
+		padding:0px;
+		padding-top:1em;
+		clear:both;
+	}
+	side-menu .text-left {
+		padding-right: 5px;
+		padding-left: 5px;
+		text-align: start;
+	}
+	side-menu .text-right {
+		padding-right: 5px;
+		padding-left: 5px;
+		text-align: right;
+	}
+	side-menu div.text-center {
+		text-align:left;
+	}
+	div.side-menu div.menu-title::before {
+		display:inline;
+	}
+	div.side-menu div.menu-title::after {
+		display:inline;
+	}
+	div.side-menu div.menu-title {
+		display:block;
+		text-align:center;
+		font-size: 16px;
+	}
+	div.side-menu div.menu-elements {
+		margin-top:5px;
+	}
+	@media all and (max-width: 1199px) and (min-width: 701px) {
+		h1 {font-size:24px;}
+		#issuerLogo {max-height:64px; max-width:140%; padding-top: 5px;}
+		#networkLogo {max-height:72px; max-width:100%; }
+		div#optGblPage { font-size : 14px; }
+		div.side-menu div.menu-title { font-size : 14px; display:block; text-align:center; }
+		.paragraph { font-size : 14px; text-align: center; }
+		.leftColumn { display:block; float:none; width:100%; }
+		.rightColumn { display:block; float:none; width:100%; margin-left:0px; margin-top: 80px; text-align: center; border: 1px solid; background-color: #EAEAEA; border-color: #C1C1C1;}
+		side-menu div.text-center { text-align:center; }
+		side-menu .text-left { padding-right: 5px; padding-left: 5px; text-align: start;}
+		side-menu .text-right { padding-right: 5px; padding-left: 5px; text-align: right;}
+	}
+	@media all and (max-width: 700px) and (min-width: 481px) {
+		h1 { font-size:18px; }
+		div#optGblPage { font-size : 14px;}
+		div.side-menu div.menu-title { font-size : 14px; display:block; text-align:center; }
+		.paragraph { font-size : 14px; text-align: center; }
+		#issuerLogo {max-height : 54px;  max-width:200%; padding-top: 10px;}
+		#networkLogo {max-height : 54px;  max-width:200%; padding-top: 10px;}
+		.leftColumn { display:block; float:none; width:100%; }
+		.rightColumn { margin-left:0px; margin-top: 80px; display:block; float:none; width:100%; text-align: center; border: 1px solid; background-color: #EAEAEA; border-color: #C1C1C1;}
+		side-menu div.text-center { text-align:center; }
+		side-menu .text-left { padding-right: 5px; padding-left: 5px; text-align: start;}
+		side-menu .text-right { padding-right: 5px; padding-left: 5px; text-align: right;}
+	}
+	@media all and (max-width: 480px) {
+		h1 { font-size:16px; }
+		div#optGblPage { font-size : 14px;}
+		div.side-menu div.menu-title { font-size : 14px; display:block; text-align:center; }
+		.paragraph { font-size : 14px; text-align: center; }
+		#issuerLogo { max-height : 42px;  max-width:250%; padding-top: 10px;}
+		#networkLogo {max-height : 42px;  max-width:250%; padding-top: 10px;}
+		.leftColumn { display:block; float:none; width:100%; }
+		.rightColumn { display:block; float:none; width:100%; margin-left:0px; margin-top: 105px; text-align: center; border: 1px solid; background-color: #EAEAEA; border-color: #C1C1C1;}
+		side-menu div.text-center { text-align:center; }
+		side-menu .text-left { padding-right: 5px; padding-left: 5px; text-align: start;}
+		side-menu .text-right { padding-right: 5px; padding-left: 5px; text-align: right;}
+		#footer { margin-top: 7px; margin-bottom : 10px; padding-top: 6px; padding-bottom: 6px; }
+		#pageHeader { height: 75px; }
+	}
+	@media all and (max-width: 347px) {
+		h1 { font-size:14px; }
+		div#optGblPage { font-size : 12px; }
+		div.side-menu div.menu-title { font-size : 12px; display:block; text-align:center; }
+		.paragraph { font-size : 12px; text-align: center; }
+		.rightColumn { display:block; float:none; width:100%; margin-left:0px; margin-top: 90px; text-align: center; border: 1px solid; background-color: #EAEAEA; border-color: #C1C1C1;}
+		#pageHeader {height: 60px;}
+	}
+	@media all and (max-width: 309px) {
+		h1 { font-size:12px; }
+		div#optGblPage { font-size : 10px; }
+		div.side-menu div.menu-title { font-size : 10px; display:block; text-align:center; }
+		.paragraph { font-size : 10px; text-align: center; }
+		.rightColumn { display:block; float:none; width:100%; margin-left:0px; margin-top: 80px; text-align: center; border: 1px solid; background-color: #EAEAEA; border-color: #C1C1C1;}
+	}
+	@media all and (max-width: 250px) {
+		h1 { font-size:10px; }
+		div#optGblPage { font-size : 8px; }
+		div.side-menu div.menu-title { font-size : 8px; display:block; text-align:center; }
+		.paragraph { font-size : 8px; text-align: center; }
+		.rightColumn { display:block; float:none; width:100%; margin-left:0px; margin-top: 70px; text-align: center; border: 1px solid; background-color: #EAEAEA; border-color: #C1C1C1;}
+	}
+</style>
+<div id="optGblPage">
+	<div id="pageHeader" ng-style="style" class="ng-scope">
+		<div id="pageHeaderLeft" ng-style="style" class="ng-scope">
+			<custom-image alt-key="''network_means_pageType_1_IMAGE_ALT''" image-key="''network_means_pageType_1_IMAGE_DATA''" id="issuerLogo" straight-mode="false"></custom-image>
+		</div>
+		<div id="pageHeaderCenter" ng-style="style" class="ng-scope"></div>
+		<div id="pageHeaderRight" ng-style="style" class="ng-scope" >
+			<custom-image alt-key="''network_means_pageType_2_IMAGE_ALT''"  image-key="''network_means_pageType_2_IMAGE_DATA''" id="networkLogo" straight-mode="false"></custom-image>
+		</div>
+	</div>
+	
+	<alternative-display attribute="''externalWSResponse''" value="''UNKNOWN_USER''"
+        enabled="''unknownUser''"
+        disabled="''defaultContent''"
+        default-fallback="''defaultContent''">
+   </alternative-display>
+   
+   <!-- Display - REFUSAL_CAUSE : UNKNOWN_USER -->
+   <div class="unknownUser" style="display: none;">
+    <message-banner 
+		heading-attr="''network_means_pageType_22''" 
+		message-attr="''network_means_pageType_201''"
+		close-button="''network_means_pageType_174''" back-button="''network_means_pageType_175''">
+	</message-banner>
+   </div>
+   
+   <div class="defaultContent" style="display: none;">
+	<message-banner
+		heading-attr="''network_means_pageType_22''"
+		message-attr="''network_means_pageType_23''"
+		close-button="''network_means_pageType_174''" back-button="''network_means_pageType_175''">
+	</message-banner>
+   </div>
+
+	<div id="displayLayout" class="row">
+		<div id="green-banner"></div>
+	</div>
+	<style>
+		div#green-banner {
+			height: 50px !important;
+			background-color: #FFFFFF;
+			border-bottom: 5px solid #FFFFFF;
+			width: 100%;
+		}
+	</style>
+	<div class="contentRow">
+		<div x-ms-format-detection="none" class="leftColumn">
+			<side-menu menu-title="''<b>Zahlungsdetails</b>''"></side-menu>
+		</div>
+		<div class="rightColumn">
+			<div class="paragraph">
+				<custom-text custom-text-key="''network_means_pageType_1''" id="paragraph1"></custom-text>
+			</div>
+		</div>
+	</div>
+	<div id="footer"></div>
+</div>' WHERE `fk_id_layout` = @customPageLayoutRefusal;
