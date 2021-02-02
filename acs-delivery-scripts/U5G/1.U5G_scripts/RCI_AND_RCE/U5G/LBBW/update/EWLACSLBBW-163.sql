839d8683976a713d3e1df20d5cd33beb38d1ef90
Gevorg Aghajanyan <gevorga@energizeglobal.com>
2021-02-02 12:55:54 +0400

    [EWLACSLBBW-163]-refactoring set network to NULL

--- acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/LBBW/update/EWLACSLBBW-163.sql
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/LBBW/update/EWLACSLBBW-163.sql
@@ -1,7 +1,6 @@
 USE U5G_ACS_BO;
 
-SET @MaestroVID = (SELECT `id` FROM `Network` WHERE `code` = 'VISA');
-SET @MaestroMID = (SELECT `id` FROM `Network` WHERE `code` = 'MASTERCARD');
+SET @MaestroVID = NULL;
 
 SET @ordinal = 35;
 SET @customItemSetSMS = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_LBBW_OTP_SMS');
@@ -16,9 +15,7 @@ INSERT INTO `CustomItem` (`DTYPE`, `createdBy`, `creationDate`, `description`, `
                           `fk_id_network`, `fk_id_image`, `fk_id_customItemSet`)
 VALUES
     ('T',  'A709391', NOW(), null, null, null, 'VISA_OTP_SMS_OTP_FORM_PAGE_34', 'PUSHED_TO_CONFIG',
-    'de', @ordinal, 'OTP_FORM_PAGE', 'SMS wird versendet.', @MaestroVID, null, @customItemSetSMSunified),
-    ('T',  'A709391', NOW(), null, null, null, 'VISA_OTP_SMS_OTP_FORM_PAGE_34', 'PUSHED_TO_CONFIG',
-       'de', @ordinal, 'OTP_FORM_PAGE', 'SMS wird versendet.', @MaestroMID, null, @customItemSetSMSunified);
+    'de', @ordinal, 'OTP_FORM_PAGE', 'SMS wird versendet.', @MaestroVID, null, @customItemSetSMSunified);
 
 
 SET @ordinal = 35;
@@ -27,8 +24,4 @@ INSERT INTO `CustomItem` (`DTYPE`, `createdBy`, `creationDate`, `description`, `
                           `fk_id_network`, `fk_id_image`, `fk_id_customItemSet`)
 VALUES
     ('T',  'A709391', NOW(), null, null, null, 'VISA_OTP_SMS_OTP_FORM_PAGE_35', 'PUSHED_TO_CONFIG',
-    'de', @ordinal, 'OTP_FORM_PAGE', 'Bitte haben Sie einen kleinen Moment Geduld. In Kürze erhalten Sie eine neue mTAN. Alle vorherigen mTANs sind nicht mehr gültig.', @MaestroVID, null, @customItemSetSMSunified),
-    ('T',  'A709391', NOW(), null, null, null, 'VISA_OTP_SMS_OTP_FORM_PAGE_35', 'PUSHED_TO_CONFIG',
-    'de', @ordinal, 'OTP_FORM_PAGE', 'Bitte haben Sie einen kleinen Moment Geduld. In Kürze erhalten Sie eine neue mTAN. Alle vorherigen mTANs sind nicht mehr gültig.', @MaestroMID, null, @customItemSetSMSunified);
-
-
+    'de', @ordinal, 'OTP_FORM_PAGE', 'Bitte haben Sie einen kleinen Moment Geduld. In Kürze erhalten Sie eine neue mTAN. Alle vorherigen mTANs sind nicht mehr gültig.', @MaestroVID, null, @customItemSetSMSunified);
