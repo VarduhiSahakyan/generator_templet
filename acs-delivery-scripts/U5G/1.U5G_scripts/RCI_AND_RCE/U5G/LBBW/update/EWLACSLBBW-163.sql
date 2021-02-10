a8219917ed42e879df101a103411be6611c00b12
Gevorg Aghajanyan <gevorga@energizeglobal.com>
2021-02-10 14:46:42 +0400

    [EWLACSLBBW-163]-PW + SMS - OTP Resent Banner wrong (refactoring)

--- acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/LBBW/update/EWLACSLBBW-163.sql
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/LBBW/update/EWLACSLBBW-163.sql
@@ -1,30 +1,7 @@
 USE U5G_ACS_BO;
 
-SET @createdBy = 'A709391';
-SET @MaestroVID = NULL;
-SET @MaestroVName = (SELECT `name` FROM `Network` WHERE `code` = 'VISA');
-
--- Elements for the profile PASSWORD
-
-SET @currentAuthentMean = 'PASSWORD';
-SET @currentPageType = 'OTP_FORM_PAGE';
-
-SET @customItemSetPassword = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_LBBW_PASSWORD_UNIFIED');
-
-INSERT INTO `CustomItem` (`DTYPE`, `createdBy`, `creationDate`, `description`, `lastUpdateBy`, `lastUpdateDate`,
-                          `name`, `updateState`, `locale`, `ordinal`, `pageTypes`, `value`,
-                          `fk_id_network`, `fk_id_image`, `fk_id_customItemSet`) VALUES
-('T',  @createdBy, NOW(), null, null, null, CONCAT(@MaestroVName,'_',@currentAuthentMean,'_',@currentPageType,'_34'), 'PUSHED_TO_CONFIG',
- 'de', 34, @currentPageType, 'SMS wird versendet.', @MaestroVID, null, @customItemSetPassword),
-('T',  @createdBy, NOW(), null, null, null, CONCAT(@MaestroVName,'_',@currentAuthentMean,'_',@currentPageType,'_35'), 'PUSHED_TO_CONFIG',
- 'de', 35, @currentPageType, 'Bitte haben Sie einen kleinen Moment Geduld. In Kürze erhalten Sie eine neue mTAN. Alle vorherigen mTANs sind nicht mehr gültig.', @MaestroVID, null, @customItemSetPassword);
-
-
--- SMS --
-
-SET @customItemSetSMS = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_LBBW_OTP_SMS');
+SET @customItemSetSMS = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_LBBW_OTP_SMS_UNIFIED');
 
 SET @ordinal = 35;
 update CustomItem set value = 'Bitte haben Sie einen kleinen Moment Geduld. In Kürze erhalten Sie eine neue mTAN. Alle vorherigen mTANs sind nicht mehr gültig.'
 where fk_id_customItemSet = @customItemSetSMS and ordinal = @ordinal;
-
--- acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/LBBW/update/Rollback_EWLACSLBBW-163.sql
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/LBBW/update/Rollback_EWLACSLBBW-163.sql
@@ -1,11 +1,8 @@
 USE U5G_ACS_BO;
 
--- Password --
-SET @customItemSetPassword = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_LBBW_PASSWORD_UNIFIED');
-delete from CustomItem where fk_id_customItemSet = @customItemSetPassword and pageTypes = 'OTP_FORM_PAGE' and ordinal in (34, 35);
 
+SET @customItemSetSMS = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_LBBW_OTP_SMS_UNIFIED');
 
-SET @customItemSetSMS = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_LBBW_OTP_SMS');
 SET @ordinal = 35;
 update CustomItem set value = 'Bitte haben Sie einen kleinen Moment Geduld. In Kürze erhalten Sie eine neue mTAN.'
 where fk_id_customItemSet = @customItemSetSMS and ordinal = @ordinal;
