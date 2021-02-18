1bcce3173d14c109e2ab4219e70852b15fe08fdc
Emma Abgaryan <emmaa@tce.loc>
2021-02-18 11:51:45 +0100

    [EWLACS3RBK-107] default refusal page Mastercard logo should be Mastercard ID Check and page title text is missing

---
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/ReiseBank/Rollback_06_EWLACS3RBK-107.sql
@@ -0,0 +1,77 @@
+use U5G_ACS_BO;
+
+SET @createdBy ='A758582';
+SET @BankUB = 'ReiseBank';
+SET @subIssuerCode = '12000';
+
+/* CustomItem */
+SET @customItemSetREFUSAL = (SELECT id FROM `CustomItemSet` WHERE `name` = CONCAT('customitemset_', @subIssuerCode, '_', @BankUB, '_DEFAULT_REFUSAL'));
+SET @MasterCardId = (SELECT `id` FROM `Image` WHERE `name` = 'MC_LOGO');
+SET @locale = 'de';
+SET @ordinal = 20;
+SET @pageTypes = 'REFUSAL_PAGE';
+SET @value = '';
+UPDATE `CustomItem` SET `value` = @value WHERE `fk_id_customItemSet` = @customItemSetREFUSAL
+                                           AND `pageTypes` = @pageTypes
+                                           AND `locale` = @locale
+                                           AND `ordinal` = @ordinal;
+
+SET @pageTypes = 'ALL';
+SET @ordinal = 2;
+UPDATE `CustomItem` SET `fk_id_image` =@MasterCardId  WHERE `fk_id_customItemSet` = @customItemSetREFUSAL
+                                                      AND `pageTypes` = @pageTypes
+                                                      AND `locale` = @locale
+                                                      AND `ordinal` = @ordinal
+                                                      AND `name` = 'Mastercard Logo';
+
+SET @locale = 'en';
+SET @ordinal = 20;
+SET @pageTypes = 'REFUSAL_PAGE';
+SET @value = '';
+UPDATE `CustomItem` SET `value` = @value WHERE `fk_id_customItemSet` = @customItemSetREFUSAL
+                                           AND `pageTypes` = @pageTypes
+                                           AND `locale` = @locale
+                                           AND `ordinal` = @ordinal;
+
+SET @pageTypes = 'ALL';
+SET @ordinal = 2;
+UPDATE `CustomItem` SET `fk_id_image` =@MasterCardId  WHERE `fk_id_customItemSet` = @customItemSetREFUSAL
+                                                      AND `pageTypes` = @pageTypes
+                                                      AND `locale` = @locale
+                                                      AND `ordinal` = @ordinal
+                                                      AND `name` = 'Mastercard Logo';
+
+SET @customItemSetREFUSALFRAUD = (SELECT id FROM `CustomItemSet` WHERE `name` = CONCAT('customitemset_', @subIssuerCode, '_', @BankUB, '_DEFAULT_FRAUD'));
+SET @locale = 'de';
+SET @ordinal = 20;
+SET @pageTypes = 'REFUSAL_PAGE';
+SET @value = '';
+UPDATE `CustomItem` SET `value` = @value WHERE `fk_id_customItemSet` = @customItemSetREFUSALFRAUD
+                                           AND `pageTypes` = @pageTypes
+                                           AND `locale` = @locale
+                                           AND `ordinal` = @ordinal;
+
+SET @pageTypes = 'ALL';
+SET @ordinal = 2;
+UPDATE `CustomItem` SET `fk_id_image` =@MasterCardId  WHERE `fk_id_customItemSet` = @customItemSetREFUSALFRAUD
+                                                      AND `pageTypes` = @pageTypes
+                                                      AND `locale` = @locale
+                                                      AND `ordinal` = @ordinal
+                                                      AND `name` = 'Mastercard Logo';
+
+SET @locale = 'en';
+SET @ordinal = 20;
+SET @pageTypes = 'REFUSAL_PAGE';
+SET @value = '';
+UPDATE `CustomItem` SET `value` = @value WHERE `fk_id_customItemSet` = @customItemSetREFUSALFRAUD
+                                           AND `pageTypes` = @pageTypes
+                                           AND `locale` = @locale
+                                           AND `ordinal` = @ordinal;
+
+SET @pageTypes = 'ALL';
+SET @ordinal = 2;
+UPDATE `CustomItem` SET `fk_id_image` =@MasterCardId  WHERE `fk_id_customItemSet` = @customItemSetREFUSALFRAUD
+                                                      AND `pageTypes` = @pageTypes
+                                                      AND `locale` = @locale
+                                                      AND `ordinal` = @ordinal
+                                                      AND `name` = 'Mastercard Logo';
