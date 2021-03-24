cf82c385fc478c43dd00f86a927b96194aae3cf6
Gevorg Aghajanyan <gevorga@energizeglobal.com>
2021-03-24 12:48:30 +0400

    [EWLACSLBBW-218]-Change mTAN Text

---
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/LBBW/Rollback_12_EWLACSLBBW-218.sql
@@ -0,0 +1,10 @@
+USE U5G_ACS_BO;
+
+SET @customItemSet_LBBW_OTP_SMS = (SELECT id FROM CustomItemSet WHERE name = 'customitemset_LBBW_OTP_SMS');
+
+SET @pageType ='MESSAGE_BODY';
+
+SET @ordinal = 0;
+SET @text = 'Ihre SMS-TAN für die Zahlung bei @merchant über @amount lautet: @otp';
+UPDATE `CustomItem` SET `value` = @text
+WHERE fk_id_customItemSet = @customItemSet_LBBW_OTP_SMS and ordinal = @ordinal and pageTypes = @pageType;
