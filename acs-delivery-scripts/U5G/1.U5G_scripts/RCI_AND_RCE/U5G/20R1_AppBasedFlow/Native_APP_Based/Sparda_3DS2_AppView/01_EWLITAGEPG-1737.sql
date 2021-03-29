3a53ddc34534d0a3cf28bdae44c3a61e8d5e66f4
Vahe Hovsepyan <vaheh@tce.loc>
2021-03-29 18:12:28 +0400

    [EWLITAGEPG-1737][SPD] updated PWD page main text for appBased flow

---
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/20R1_AppBasedFlow/Native_APP_Based/Sparda_3DS2_AppView/01_EWLITAGEPG-1737.sql
@@ -0,0 +1,9 @@
+USE U5G_ACS_BO;
+
+SET @cisPWD = (SELECT `id` FROM `CustomItemSet` where name = 'customitemset_SPB_sharedBIN_PASSWORD');
+SET @pageType = 'APP_VIEW';
+SET @ordinal = 152;
+SET @text = 'Bitte geben Sie die zu der angezeigten Kundennummer passende Online-PIN ein, wie Sie es von Ihrem SpardaOnline-Banking gewohnt sind. Um fortzufahren, bestätigen Sie die Eingabe mit "Senden"';
+UPDATE `CustomItem` SET `value` = @text WHERE `fk_id_customItemSet` = @cisPWD AND
+                                              `ordinal` = @ordinal AND
+                                              `pageTypes` = @pageType;
