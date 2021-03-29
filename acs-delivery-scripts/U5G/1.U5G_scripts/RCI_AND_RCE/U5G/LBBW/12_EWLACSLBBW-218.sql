18e44f13117c8cbef61215664f48ce1198e6143f
Gevorg Aghajanyan <gevorga@energizeglobal.com>
2021-03-29 14:32:55 +0400

    [EWLACSLBBW-218]-Change mTAN Text

--- acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/LBBW/12_EWLACSLBBW-218.sql
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/LBBW/12_EWLACSLBBW-218.sql
@@ -1,10 +1,15 @@
 USE U5G_ACS_BO;
 
-SET @customItemSet_LBBW_OTP_SMS = (SELECT id FROM CustomItemSet WHERE name = 'customitemset_LBBW_OTP_SMS');
-
 SET @pageType ='MESSAGE_BODY';
 
 SET @ordinal = 0;
 SET @text = 'Ihre SMS-mTAN für die Zahlung bei @merchant über @amount lautet: @otp';
+
+SET @customItemSet_LBBW_OTP_SMS = (SELECT id FROM CustomItemSet WHERE name = 'customitemset_LBBW_OTP_SMS');
 UPDATE `CustomItem` SET `value` = @text
 WHERE fk_id_customItemSet = @customItemSet_LBBW_OTP_SMS and ordinal = @ordinal and pageTypes = @pageType;
+
+
+SET @customItemSet_LBBW_PWD = (SELECT id FROM CustomItemSet WHERE name = 'customitemset_LBBW_PASSWORD_UNIFIED');
+UPDATE `CustomItem` SET `value` = @text
+WHERE fk_id_customItemSet = @customItemSet_LBBW_PWD and ordinal = @ordinal and pageTypes = @pageType;
