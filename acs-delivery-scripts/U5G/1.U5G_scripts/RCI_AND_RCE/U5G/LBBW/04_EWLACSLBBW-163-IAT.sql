c4b588d15fde9caf6d9df790df82b095a45a618a
Vahe Hovsepyan <vaheh@tce.loc>
2021-04-07 22:35:03 +0400

    [EWLACSLBBW-223_224_225_226_227_228] update LBBW PWD+OTP templates and texts

--- acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/LBBW/04_EWLACSLBBW-163-IAT.sql
+++
@@ -1,7 +0,0 @@
-USE U5G_ACS_BO;
-
-SET @customItemSetSMS = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_LBBW_OTP_SMS');
-
-SET @ordinal = 35;
-update CustomItem set value = 'Bitte haben Sie einen kleinen Moment Geduld. In Kürze erhalten Sie eine neue mTAN. Alle vorherigen mTANs sind nicht mehr gültig.'
-where fk_id_customItemSet = @customItemSetSMS and ordinal = @ordinal;
--- acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U5G/LBBW/Rollback_11_EWLACSLBBW-203.sql
+++
@@ -1,7 +0,0 @@
-USE U5G_ACS_BO;
-
-SET @customItemSetPassword = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_LBBW_PASSWORD_UNIFIED');
-
-SET @pageType = 'OTP_FORM_PAGE';
-
-delete from CustomItem where fk_id_customItemSet = @customItemSetPassword and pageTypes = @pageType and ordinal in (28, 29);
