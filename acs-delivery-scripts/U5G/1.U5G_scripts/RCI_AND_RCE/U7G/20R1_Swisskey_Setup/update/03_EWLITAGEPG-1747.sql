ec73e6bf0c55d1ced52986a3bb6d6bcb5c8cc670
Vahe Hovsepyan <vaheh@tce.loc>
2021-04-13 09:12:37 +0400

    [EWLITAGEPG-1747]time out session low on U7G[LUKB]

---
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U7G/20R1_Swisskey_Setup/update/03_EWLITAGEPG-1747.sql
@@ -0,0 +1,4 @@
+
+USE `U7G_ACS_BO`;
+
+Update `SubIssuer` set `authenticationTimeOut` = 300 where `code` = 77800;
---
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U7G/20R1_Swisskey_Setup/update/Rollback_EWLITAGEPG-1747.sql
@@ -0,0 +1,4 @@
+
+USE `U7G_ACS_BO`;
+
+Update `SubIssuer` set `authenticationTimeOut` = 120 where `code` = 77800;
