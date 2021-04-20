2990a31bde3aaee23f2e6660c82367c91a4170b0
Vahe Hovsepyan <vaheh@tce.loc>
2021-04-20 13:46:12 +0400

    [EWLITAGEPG-974][SWK] added rollback for U0P

---
+++ acs-delivery-scripts/U5G/1.U5G_scripts/RCI_AND_RCE/U7G/20R1_Swisskey_Setup/update/EWLITAGEPG-974/Rollback-3.update-remove-Swisskey_subissuer.sql
@@ -0,0 +1,28 @@
+/*!40101 set names utf8 */;
+/*!50503 set names utf8mb4 */;
+use `U0P_ACS_WS`;
+
+set @BankB = 'Swisskey AG';
+set @subIssuerSWISSKEY = 'Swisskey AG';
+set @SubIssuerCodeBEKB = '41001';
+
+set FOREIGN_KEY_CHECKS = 0;
+
+set @parentId = (select c.id
+				 from `Customer` c
+				 where c.name = @BankB
+				   and c.customerType = 'ISSUER');
+set @id_customer = (select id
+					from Customer
+					where code = @SubIssuerCodeBEKB
+					  and customerType = 'SUB_ISSUER'
+					  and name = @subIssuerSWISSKEY
+					  and parent_id = @parentID);
+
+
+
+delete from Role_Customer where id_customer = (@id_customer);
+
+delete from Customer where id = @id_customer;
+
+set FOREIGN_KEY_CHECKS = 1;
