4a5be996db1cfd6078c555cd0b7ac7daffb6b64d
Vahe Hovsepyan <vaheh@tce.loc>
2021-05-10 13:40:21 +0400

    removed unused scripts

--- acs-delivery-scripts/SCRIPTS/RCI_AND_RCE/U5G/19R3_COBA_Setup/Coba_Inhouse/Update/01_EWLACS3CMI-153.sql
+++
@@ -1,4 +0,0 @@
-
-USE `U5G_ACS_BO`;
-
-UPDATE `SubIssuer` set `authenticationTimeOut` = 240 where `code` = '19440';
--- acs-delivery-scripts/SCRIPTS/RCI_AND_RCE/U5G/3.0.6_EWB_Setup/Update/Rollback_EWLACS3EWB-74.sql
+++
@@ -1,8 +0,0 @@
-
-USE `U5G_ACS_BO`;
-
-SET @currentPageType = 'MESSAGE_BODY';
-SET @customItemSetId = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_EWB_SMS');
-
-
-UPDATE CustomItem SET VALUE = 'EWB: You have a pending transaction at @merchant for @amount. DO NOT SHARE. If valid, use OTP @otp. Call +63288881700 for concerns.' WHERE pageTypes = @currentPageType AND fk_id_customItemSet in (@customItemSetId) AND ordinal = 0 and DTYPE='T';
