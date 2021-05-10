15e89e8379dd00d51e766e834328643d577f6c9b
Vahe Hovsepyan <vaheh@tce.loc>
2021-05-10 18:26:09 +0400

    removed unused sripts

--- acs-delivery-scripts/SCRIPTS/RCI_AND_RCE/U7G/20R1_Swisskey_Setup/update/02_EWLITAGEPG-1747.sql
+++
@@ -1,4 +0,0 @@
-
-USE `U7G_ACS_BO`;
-
-Update `SubIssuer` set `authenticationTimeOut` = 300 where `code` = 77800;
--- acs-delivery-scripts/SCRIPTS/RCI_AND_RCE/U7G/20R1_Swisskey_Setup/update/Rollback_EWLITAGEPG-1747.sql
+++
@@ -1,4 +0,0 @@
-
-USE `U7G_ACS_BO`;
-
-Update `SubIssuer` set `authenticationTimeOut` = 120 where `code` = 77800;
