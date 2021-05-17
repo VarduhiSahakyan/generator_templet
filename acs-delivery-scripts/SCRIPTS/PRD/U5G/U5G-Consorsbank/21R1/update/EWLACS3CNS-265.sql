73fd57166190656554d5e6b19adcfad30d1898e3
vahem <vahe.melkonyan.external@atos.net>
2021-05-17 17:05:49 +0400

    Setup QR-TAN Fallback for BNP WM

---
+++ acs-delivery-scripts/SCRIPTS/PRD/U5G/U5G-Consorsbank/21R1/update/EWLACS3CNS-265.sql
@@ -0,0 +1,525 @@
+USE U5G_ACS_BO;
+
+SET @BankUB = 'BNP_WM';
+SET @createdBy = 'W100851';
+SET @customPageLayoutDesc_appView = 'Polling Page (BNP_WM)';
+SET @polingPageType ='POLLING_PAGE';
+SET @authentMean = 'OTP';
+SET @MaestroVID = (SELECT id FROM `Network` WHERE code = 'VISA');
+SET @updateState = 'PUSHED_TO_CONFIG';
+SET @profilePhotoTan = (SELECT id FROM `Profile` WHERE `name` = CONCAT(@BankUB, '_PHOTO_TAN_01'));
+SET @rulePhotoTan = (SELECT id FROM `Rule` WHERE `description` = 'PHOTO_TAN_AVAILABLE_NORMAL' AND `fk_id_profile` = @profilePhotoTan);
+SET @subIssuerID = (SELECT id FROM `SubIssuer` WHERE `code` = '16901' AND `name` = 'BNP Paribas Wealth Management');
+SET @authMeanUNDEFINED = (SELECT id FROM `AuthentMeans` WHERE `name` = 'UNDEFINED');
+SET @authentMeansMobileAppExt = (SELECT id FROM `AuthentMeans`	WHERE `name` = 'MOBILE_APP_EXT');
+SET @authentMeansPhotoTan = (SELECT id FROM `AuthentMeans`	WHERE `name` = 'PHOTO_TAN');
+SET @authMeanPassword = (SELECT id FROM `AuthentMeans`	WHERE `name` = 'EXT_PASSWORD');
+SET @passwordCustomItemSet = (SELECT id FROM CustomItemSet WHERE name = CONCAT('customitemset_',@BankUB,'_MOBILE_APP_EXT'));
+
+
+
+SET @idAppViewPage = (select id
+						from `CustomPageLayout`
+						where `pageType` = @polingPageType
+						and DESCRIPTION = @customPageLayoutDesc_appView);
+
+UPDATE `CustomComponent`
+SET `value` = '<custom-font straight-mode="false" mime-type-key="''network_means_pageType_301_FONT_MIME_TYPE''"
+			 title-key="''network_means_pageType_301_FONT_TITLE''"
+			 font-key="''network_means_pageType_301_FONT_DATA''"></custom-font>
+<custom-font straight-mode="false" mime-type-key="''network_means_pageType_304_FONT_MIME_TYPE''"
+			 title-key="''network_means_pageType_304_FONT_TITLE''"
+			 font-key="''network_means_pageType_304_FONT_DATA''"></custom-font>
+<custom-font straight-mode="false" mime-type-key="''network_means_pageType_305_FONT_MIME_TYPE''"
+			 title-key="''network_means_pageType_305_FONT_TITLE''"
+			 font-key="''network_means_pageType_305_FONT_DATA''"></custom-font>
+<custom-font straight-mode="false" mime-type-key="''network_means_pageType_306_FONT_MIME_TYPE''"
+			 title-key="''network_means_pageType_306_FONT_TITLE''"
+			 font-key="''network_means_pageType_306_FONT_DATA''"></custom-font>
+
+<div id="main-container">
+	<div id="header">
+		<div id="banklogo">
+			<custom-image id="issuerLogo" alt-key="''network_means_pageType_1_IMAGE_ALT''" image-key="''network_means_pageType_1_IMAGE_DATA''" straight-mode="false"></custom-image>
+		</div>
+		<div id="schemeLogo">
+			<custom-image id="networkLogo" alt-key="''network_means_pageType_2_IMAGE_ALT''" image-key="''network_means_pageType_2_IMAGE_DATA''" straight-mode="false">
+		</div>
+	</div>
+	<div id="content">
+		<message-banner close-button="''network_means_pageType_174''" back-button="''network_means_pageType_175''"></message-banner>
+			<h2>
+				<custom-text custom-text-key="''network_means_pageType_1''" id="title"></custom-text>
+			</h2>
+			<div class="transactiondetails">
+				<side-menu></side-menu>
+			</div>
+			<br></br>
+			<br></br>
+			<p></p>
+			<h2>
+				<custom-text custom-text-key="''network_means_pageType_3''" id="paragraph1"></custom-text>
+			</h2>
+			<span>
+				<custom-text custom-text-key="''network_means_pageType_4''" id="paragraph1"></custom-text>
+			</span>
+			<p></p>
+			<div class="mobileapp">
+				<custom-image id="mobileAppLogo" alt-key="''network_means_pageType_3_IMAGE_ALT''" image-key="''network_means_pageType_3_IMAGE_DATA''" straight-mode="false">
+			</div>
+	</div>
+			  <div id="form-controls">
+				<div class="row">
+					<div class="back-link">
+						<span class="fa fa-angle-left"></span>
+						<switch-means-button change-means-label="''network_means_pageType_10''" id="switchId"></switch-means-button>
+					</div>
+				</div>
+			</div>
+
+	<div id="form-controls">
+		<div class="row">
+			<div class="back-link">
+				<span class="fa fa-angle-left"></span>
+				<cancel-button cn-label="''network_means_pageType_11''" id="cancelButton" ></cancel-button>
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+		<div class="help-area">
+			<div class="help-link">
+				 <help help-label="''network_means_pageType_5''" id="helpButton" class="helpButtonClass"></help>
+				<span class="fa fa-angle-right"></span>
+			</div>
+			<div class="contact">
+				<div class="line bottom-margin">
+					  <custom-text custom-text-key="''network_means_pageType_6''"></custom-text>
+				</div>
+				<div class="line small bold">
+					<div class=""><custom-text custom-text-key="''network_means_pageType_7''"></custom-text></div>
+				</div>
+				<div class="line small grey">
+					<div class=""><custom-text custom-text-key="''network_means_pageType_8''"></custom-text></div>
+				</div>
+			</div>
+		</div>
+		<div id="copyright" class="extra-small">
+			<div><span><custom-text custom-text-key="''network_means_pageType_9''"></custom-text></span></div>
+			<div><span><custom-text custom-text-key="''network_means_pageType_44''"></custom-text></span></div>
+		</div>
+	</div>
+</div>
+<style>
+	@font-face {
+		font-family: "BNPPSans";
+		src: url(data:font/truetype;charset=utf-8;base64,<<copied base64 string>>) format(''truetype'');
+		font-weight: normal;
+		font-style: normal;
+	}
+	.mobileapp {
+		text-align: center;
+	}
+	#main-container {
+		font-family: BNPPSans;
+		font-size: 15px;
+		color: #403f3d;
+		width: 480px;
+		max-width: 480px;
+		margin-left: auto;
+		margin-right: auto;
+		padding-left: 10px;
+		padding-right: 10px;
+	}
+	#header {
+		height: 100px;
+		position: relative;
+	}
+	#main-container #networkLogo {
+		max-height: 33px;
+		max-width: 100%;
+	}
+	#main-container #issuerLogo {
+		max-height: 72px;
+		padding-left: 0px;
+		padding-right: 0px;
+	}
+	#mobileAppLogo {
+		position: relative;
+	}
+	#schemeLogo {
+		width: 100px;
+		height: 100px;
+		position: absolute;
+		right: 0px;
+		top: 1em;
+		padding-right: 1em;
+	}
+	div#contentHeader {
+		background-color: #f7f7f7;
+	}
+	div#transactionDetails {
+		background-color: #f7f7f7;
+	}
+	custom-text#title {
+		font-weight: bold;
+		font-size: 25px;
+		font-family: BNPPSans;
+		color: #403f3d;
+	}
+	#content {
+		text-align: left;
+		color: #403f3d;
+		background-color: #f7f7f7;
+	}
+	#content h2 {
+		font-family: BNPPSansLight;
+		font-size: 25px;
+		line-height: 0.8;
+		font-weight: normal;
+		margin-bottom: 0.25em;
+		margin-top: 0.25em;
+		color: #403f3d;
+		padding-top: 7px;
+	}
+	.btn {
+		border-radius: 0px;
+	}
+	span.col-sm-6.col-xs-6.text-left.padding-left {
+		font-family: BNPPSans;
+	}
+	.side-menu .text-left, .side-menu .text-right {
+		font-family: BNPPSans;
+		padding-right: 5px;
+		padding-left: 5px;
+		font-size: 15px;
+		color: #403f3d;
+	}
+	.menu-elements {
+		color: #403f3d;
+	}
+	div#form-controls {
+		background-color: #f7f7f7;
+	}
+	#main-container #footer {
+		width: 100%;
+		background-color: #d1d1d1;
+		clear: both;
+		background-image: none;
+		height: auto;
+		color: #403f3d;
+	}
+	#main-container #footer:after {
+		content: '''';
+		height: 100%;
+		display: table;
+		clear: both;
+		padding-bottom: 0.5em;
+	}
+	#main-container #footer .extra-small {
+		font-size: 12px;
+	}
+	#main-container #footer .small {
+		font-size: 0.8em;
+		font-weight: normal;
+	}
+	#main-container #footer .bold {
+		font-size: 15px;
+		font-weight: bold;
+		color: #403f3d;
+	}
+	#main-container #footer .grey {
+		color: #6b6b6b;
+		font-weight: normal;
+	}
+	#main-container #footer #copyright {
+		width: 100%;
+		border-top: 1px solid #6b6b6b;
+		padding-top: 0.5em;
+		padding-bottom: 1em;
+		display: flex;
+		flex-direction: row;
+	}
+	#main-container #footer #copyright div:nth-child(1) {
+		order: 1;
+		text-align: left;
+		width: 50%;
+		padding-left: 12px;
+	}
+	#main-container #footer #copyright div:nth-child(2) {
+		order: 2;
+		text-align: right;
+		width: 50%;
+		padding-right: 12px;
+	}
+	#main-container #footer .help-area {
+		display: flex;
+		flex-direction: row;
+		padding: 16px;
+	}
+	p {
+		margin-bottom: 10px;
+	}
+	#main-container #footer .small-font {
+		font-size: 0.75em;
+	}
+	.splashtext {
+		width: 80%;
+		margin-left: auto;
+		margin-right: auto
+	}
+	input {
+		border: 1px solid #d1d1d1;
+		color: #464646;
+		padding: 7px 10px 5px;
+		height: 20px;
+		box-shadow: rgba(0, 0, 0, 0.15) 1px 1px 3px 0 inset;
+	}
+	.spinner-container {
+		display: block;
+		width: 100%;
+	}
+	#messageBanner-innerContainer {
+		padding: 20px;
+	}
+	#messageBanner {
+		width: 100%;
+		margin-left: 0px;
+		margin-right: 0px;
+		margin-top: 10px;
+		margin-bottom: 10px;
+		background-color: #f5f5f5;
+		padding: 5px;
+		box-sizing: border-box;
+	}
+	.error {
+		color: #d00;
+	}
+	#messageBanner p {
+		margin-top: 0.25em;
+		margin-bottom: 0.25em;
+	}
+	#messageBanner h3 {
+		margin-top: 0.25em;
+		margin-bottom: 0.25em;
+	}
+	#main-container .row button {
+		font-family: BNPPSans;
+		font-size: 15px;
+		height: 38px;
+		background: linear-gradient(#4dbed3, #007ea5);
+		box-shadow: none;
+		border: 0px;
+		color: #000;
+	}
+	#main-container .row button:hover:enabled {
+		background-color: #5b7f95;
+	}
+	#switchId button span.fa {
+		display: none;
+	}
+	#switchId button span.fa-check-square {
+		display: none;
+	}
+	#cancelButton button span.fa {
+		display: none;
+	}
+	#main-container .row .left {
+		float: left;
+		width: 180px;
+		text-align: right;
+	}
+	#main-container .row .left span {
+		margin-right: 0.5em
+	}
+	#main-container .row .back-link {
+		text-align: left;
+		float: left;
+	}
+	#main-container .row .back-link button {
+		border-style: none;
+		background: none;
+		padding: 0px;
+		background-color: #f7f7f7;
+		color: #06c2d4;
+	}
+	#main-container .row .back-link span {
+		text-align: left;
+		margin-left: 0.5em;
+		color: #749bb3;
+	}
+	#main-container .row .back-link span.fa-ban {
+		display: none;
+	}
+	#main-container #footer .help-area {
+		display: flex;
+		flex-direction: row;
+		padding: 16px;
+	}
+	#main-container #helpButton button span {
+		font-family: BNPPSans;
+		font-size: 15px;
+		color: white;
+	}
+	#main-container #helpButton button {
+		border-style: none;
+		padding: 2px;
+		background-color: #5b7f95;
+	}
+	#main-container #helpButton .fa-info {
+		display: none;
+	}
+	.help-link {
+		width: 30%;
+		order: 2;
+		text-align: right
+	}
+	.contact {
+		width: 70%;
+		order: 1;
+		font-weight: bold;
+	}
+	@media (max-width: 560px) {
+		#main-container {width: auto;}
+		body {font-size: 14px;}
+		#header {height: 65px;}
+		#schemeLogo {margin-top: 1em;width: 70px;height: 70px;}
+		.row {width: auto;clear: none;}
+		.row .back-link {float: none;text-align: center;padding-top: 0.5em;}
+		.row button {width: 90%;}
+		#main-container #footer {width: 100%;background-image: none;background-color: #f5f5f5;clear: both;height: unset;}
+		#main-container #footer .help-area {display: flex;flex-direction: column;padding: 16px;text-align: center;}
+		.help-link {width: 100%;order: 2;text-align: center;padding-top: 1em;}
+		.contact {width: 100%;order: 1;}
+		#main-container #footer .small-font {font-size: 0.75em;}
+	}
+</style>'
+WHERE `fk_id_layout` = @idAppViewPage;
+
+SET @textValue = 'Mit QR-TAN bestätigen';
+
+INSERT INTO `CustomItem` (`DTYPE`, `createdBy`, `creationDate`, `description`, `lastUpdateBy`, `lastUpdateDate`,
+							`name`, `updateState`, `locale`, `ordinal`, `pageTypes`, `value`,
+							`fk_id_network`, `fk_id_image`, `fk_id_customItemSet`) VALUES
+('T', @createdBy, NOW(), NULL, NULL, NULL, CONCAT(@authentMean,'_',@polingPageType,'_10'), @updateState,
+ 'de', 10, @polingPageType,	 @textValue, @MaestroVID, NULL, @passwordCustomItemSet);
+
+
+INSERT INTO `Profile` (`createdBy`, `creationDate`, `description`, `lastUpdateBy`, `lastUpdateDate`, `name`,
+						`updateState`, `maxAttempts`,`dataEntryFormat`, `dataEntryAllowedPattern`, `fk_id_authentMeans`,
+						`fk_id_customItemSetCurrent`, `fk_id_customItemSetOld`, `fk_id_customItemSetNew`,
+						`fk_id_subIssuer`) VALUES
+(@createdBy, NOW(), 'UNDEFINED', NULL, NULL, CONCAT(@BankUB,'_UNDEFINED_01'), @updateState, 4,'6:(:DIGIT:1)','^[^OIi]*$', @authMeanUNDEFINED, NULL, NULL, NULL, @subIssuerID);
+
+SET @profileUNDEFINED = (SELECT id FROM `Profile` WHERE `name` = CONCAT(@BankUB,'_UNDEFINED_01'));
+SET @profileAccept = (SELECT id FROM Profile WHERE name = CONCAT(@BankUB, '_ACCEPT_01'));
+SET @profileRefusalDefault = (SELECT id FROM Profile WHERE name = CONCAT(@BankUB, '_REFUSAL_01'));
+
+
+UPDATE Rule SET orderRule = 7 WHERE name = 'ACCEPT MAINT' AND fk_id_profile = @profileAccept;
+UPDATE Rule SET orderRule = 9 WHERE name = 'REFUSAL (DEFAULT)' AND fk_id_profile = @profileRefusalDefault;
+
+
+INSERT INTO `Rule` (`createdBy`, `creationDate`, `description`, `lastUpdateBy`, `lastUpdateDate`, `name`,
+					`updateState`, `orderRule`, `fk_id_profile`) VALUES
+(@createdBy, NOW(), 'UNDEFINED', NULL, NULL, 'UNDEFINED', @updateState, 8, @profileUNDEFINED);
+
+
+SET @ruleUNDEFINED = (SELECT id FROM `Rule` WHERE `description`='UNDEFINED' AND `fk_id_profile`=@profileUNDEFINED);
+
+
+INSERT INTO `RuleCondition` (`createdBy`, `creationDate`, `description`, `lastUpdateBy`, `lastUpdateDate`, `name`,
+							 `updateState`, `fk_id_rule`) VALUES
+(@createdBy, NOW(), NULL, NULL, NULL, CONCAT('C1_P_', @BankUB, '_02_PHOTO_TAN'), @updateState, @rulePhotoTan),
+(@createdBy, NOW(), NULL, NULL, NULL, CONCAT('C1_P_', @BankUB, '_01_UNDEFINED'), @updateState, @ruleUNDEFINED);
+
+-- PHOTO TAN --
+
+INSERT INTO `Condition_MeansProcessStatuses` (`id_condition`, `id_meansProcessStatuses`)
+SELECT c.id, mps.id FROM `RuleCondition` c, `MeansProcessStatuses` mps
+WHERE c.`name` = CONCAT('C1_P_', @BankUB, '_02_PHOTO_TAN')
+AND mps.`fk_id_authentMean` = @authentMeansPhotoTan AND (mps.`meansProcessStatusType` = 'MEANS_AVAILABLE' AND mps.`reversed` = FALSE);
+
+INSERT INTO `Condition_MeansProcessStatuses` (`id_condition`, `id_meansProcessStatuses`)
+SELECT c.id, mps.id FROM `RuleCondition` c, `MeansProcessStatuses` mps
+WHERE c.`name` = CONCAT('C1_P_', @BankUB, '_02_PHOTO_TAN')
+AND mps.`fk_id_authentMean` = @authentMeansPhotoTan AND (mps.`meansProcessStatusType` IN ('MEANS_DISABLED') AND mps.`reversed` = TRUE);
+
+INSERT INTO `Condition_MeansProcessStatuses` (`id_condition`, `id_meansProcessStatuses`)
+SELECT c.id, mps.id FROM `RuleCondition` c, `MeansProcessStatuses` mps
+WHERE c.`name`= CONCAT('C1_P_',@BankUB,'_02_PHOTO_TAN')
+AND mps.`fk_id_authentMean`=@authentMeansPhotoTan AND (mps.`meansProcessStatusType`='HUB_AUTHENTICATION_MEAN_AVAILABLE' AND mps.`reversed`=FALSE);
+
+INSERT INTO `Condition_MeansProcessStatuses` (`id_condition`, `id_meansProcessStatuses`)
+SELECT c.id, mps.id FROM `RuleCondition` c, `MeansProcessStatuses` mps
+WHERE c.`name`= CONCAT('C1_P_',@BankUB,'_02_PHOTO_TAN')
+AND mps.`fk_id_authentMean`=@authMeanPassword AND (mps.`meansProcessStatusType` IN ('COMBINED_MEANS_REQUIRED') AND mps.`reversed`=FALSE);
+
+INSERT INTO `Condition_MeansProcessStatuses` (`id_condition`, `id_meansProcessStatuses`)
+SELECT c.id, mps.id FROM `RuleCondition` c, `MeansProcessStatuses` mps
+WHERE c.`name` = CONCAT('C1_P_', @BankUB, '_02_PHOTO_TAN')
+AND mps.`fk_id_authentMean` = @authentMeansMobileAppExt AND (mps.`meansProcessStatusType` = 'COMBINED_MEANS_REQUIRED' AND mps.`reversed` = TRUE);
+
+INSERT INTO `Condition_MeansProcessStatuses` (`id_condition`, `id_meansProcessStatuses`)
+SELECT c.id, mps.id FROM `RuleCondition` c, `MeansProcessStatuses` mps
+WHERE c.`name` = CONCAT('C1_P_', @BankUB, '_02_PHOTO_TAN')
+AND mps.`fk_id_authentMean` = @authentMeansPhotoTan AND (mps.`meansProcessStatusType` = 'COMBINED_MEANS_REQUIRED' AND mps.`reversed` = TRUE);
+
+INSERT INTO `Condition_TransactionStatuses` (`id_condition`, `id_transactionStatuses`)
+SELECT c.id, ts.id FROM `RuleCondition` c, `TransactionStatuses` ts
+WHERE c.`name`= CONCAT('C1_P_',@BankUB,'_02_PHOTO_TAN')
+AND (ts.`transactionStatusType`='COMBINED_AUTHENTICATION_ALLOWED' AND ts.`reversed`=FALSE);
+
+INSERT INTO `Condition_MeansProcessStatuses` (`id_condition`, `id_meansProcessStatuses`)
+SELECT c.id, mps.id FROM `RuleCondition` c, `MeansProcessStatuses` mps
+WHERE c.`name` = CONCAT('C1_P_', @BankUB, '_02_PHOTO_TAN')
+AND mps.`fk_id_authentMean` = @authentMeansMobileAppExt
+AND (mps.`meansProcessStatusType` = 'USER_CHOICE_DEMANDED' AND mps.`reversed` = FALSE);
+
+SET @ruleConditionPhotoTan = (SELECT `id` FROM `RuleCondition` WHERE `fk_id_rule` = @rulePhotoTan AND name = 'C1_P_BNP_WM_02_PHOTO_TAN');
+
+INSERT INTO `TransactionValue` (`reversed`, `transactionValueType`, `value`, `fk_id_condition`) VALUES
+	(b'1', 'DEVICE_CHANNEL', '01', @ruleConditionPhotoTan );
+
+-- UNDEFINED --
+
+INSERT INTO `Condition_MeansProcessStatuses` (`id_condition`, `id_meansProcessStatuses`)
+SELECT c.id, mps.id FROM `RuleCondition` c, `MeansProcessStatuses` mps
+WHERE c.`name`=CONCAT('C1_P_',@BankUB,'_01_UNDEFINED')
+AND mps.`fk_id_authentMean`=@authMeanUNDEFINED AND (mps.`meansProcessStatusType`='MEANS_AVAILABLE' AND mps.`reversed`=FALSE);
+
+INSERT INTO `Condition_MeansProcessStatuses` (`id_condition`, `id_meansProcessStatuses`)
+SELECT c.id, mps.id FROM `RuleCondition` c, `MeansProcessStatuses` mps
+WHERE c.`name`=CONCAT('C1_P_',@BankUB,'_01_UNDEFINED')
+AND mps.`fk_id_authentMean`=@authentMeansMobileAppExt AND (mps.`meansProcessStatusType`='MEANS_AVAILABLE' AND mps.`reversed`=FALSE);
+
+INSERT INTO `Condition_MeansProcessStatuses` (`id_condition`, `id_meansProcessStatuses`)
+SELECT c.id, mps.id FROM `RuleCondition` c, `MeansProcessStatuses` mps
+WHERE c.`name`=CONCAT('C1_P_',@BankUB,'_01_UNDEFINED')
+AND mps.`fk_id_authentMean`=@authentMeansMobileAppExt AND (mps.`meansProcessStatusType`='MEANS_DISABLED' AND mps.`reversed`=TRUE);
+
+INSERT INTO `Condition_MeansProcessStatuses` (`id_condition`, `id_meansProcessStatuses`)
+SELECT c.id, mps.id FROM `RuleCondition` c, `MeansProcessStatuses` mps
+WHERE c.`name`=CONCAT('C1_P_',@BankUB,'_01_UNDEFINED')
+AND mps.`fk_id_authentMean`=@authentMeansMobileAppExt AND (mps.`meansProcessStatusType`='HUB_AUTHENTICATION_MEAN_AVAILABLE' AND mps.`reversed`=FALSE);
+
+INSERT INTO `Condition_MeansProcessStatuses` (`id_condition`, `id_meansProcessStatuses`)
+SELECT c.id, mps.id FROM `RuleCondition` c, `MeansProcessStatuses` mps
+WHERE c.`name`=CONCAT('C1_P_',@BankUB,'_01_UNDEFINED')
+AND mps.`fk_id_authentMean`=@authentMeansPhotoTan AND (mps.`meansProcessStatusType`='MEANS_AVAILABLE' AND mps.`reversed`=FALSE);
+
+INSERT INTO `Condition_MeansProcessStatuses` (`id_condition`, `id_meansProcessStatuses`)
+SELECT c.id, mps.id FROM `RuleCondition` c, `MeansProcessStatuses` mps
+WHERE c.`name`=CONCAT('C1_P_',@BankUB,'_01_UNDEFINED')
+AND mps.`fk_id_authentMean`=@authentMeansPhotoTan AND (mps.`meansProcessStatusType`='MEANS_DISABLED' AND mps.`reversed`=TRUE);
+
+INSERT INTO `Condition_MeansProcessStatuses` (`id_condition`, `id_meansProcessStatuses`)
+SELECT c.id, mps.id FROM `RuleCondition` c, `MeansProcessStatuses` mps
+WHERE c.`name`=CONCAT('C1_P_',@BankUB,'_01_UNDEFINED')
+AND mps.`fk_id_authentMean`=@authentMeansPhotoTan AND (mps.`meansProcessStatusType`='HUB_AUTHENTICATION_MEAN_AVAILABLE' AND mps.`reversed`=FALSE);
+
+
+SET @ruleUndefined = (SELECT id FROM `Rule` WHERE `description` = 'UNDEFINED' AND `fk_id_profile` = @profileUNDEFINED);
+SET @RuleId = (SELECT  r.`id` FROM	`Rule` r WHERE	r.`id` = @ruleUndefined);
+SET @ProfileSet = (SELECT id FROM `ProfileSet` WHERE `name` = 'PS_BNP_WM_01');
+INSERT INTO `ProfileSet_Rule` (`id_profileSet`, `id_rule`) VALUES
+(@ProfileSet , @RuleId);
