523123ab9ca75771c4bbe34c937476d43d26f5d3
Gevorg Aghajanyan <gevorga@energizeglobal.com>
2021-05-20 14:57:57 +0400

    [EWLACS3CMC-151]-Update Template Texts and Add Help-Texts

---
+++ acs-delivery-scripts/SCRIPTS/RCI_AND_RCE/U5G/19R3_COBA_Setup/Coba_Cobrand/updates/Rollback_03_EWLACS3CMC-151.sql
@@ -0,0 +1,43 @@
+USE U5G_ACS_BO;
+
+SET @customItemSetPASSWORD = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_COB_PASSWORD');
+
+SET @currentPageType = 'OTP_FORM_PAGE';
+
+SET @ordinal = 12;
+update CustomItem set value = 'Bitte warten Sie einige Sekunden.'
+where fk_id_customItemSet = @customItemSetPASSWORD and pageTypes = @currentPageType and ordinal = @ordinal;
+
+SET @ordinal = 13;
+update CustomItem set value = 'Bitte warten Sie einige Sekunden. Ihre Eingabe wird geprüft. '
+where fk_id_customItemSet = @customItemSetPASSWORD and pageTypes = @currentPageType and ordinal = @ordinal;
+
+SET @ordinal = 14;
+update CustomItem set value = 'Die Zahlung wurde abgebrochen.'
+where fk_id_customItemSet = @customItemSetPASSWORD and pageTypes = @currentPageType and ordinal = @ordinal;
+
+SET @ordinal = 15;
+update CustomItem set value = 'Starten Sie den Bezahlvorgang erneut, wenn Sie die Zahlung durchführen möchten.'
+where fk_id_customItemSet = @customItemSetPASSWORD and pageTypes = @currentPageType and ordinal = @ordinal;
+
+
+-- SMS --
+SET @customItemSetSMS = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_COB_SMS');
+
+SET @currentPageType = 'OTP_FORM_PAGE';
+
+SET @ordinal = 12;
+update CustomItem set value = 'Bitte warten Sie einige Sekunden.'
+where fk_id_customItemSet = @customItemSetSMS and pageTypes = @currentPageType and ordinal = @ordinal;
+
+SET @ordinal = 13;
+update CustomItem set value = 'Bitte warten Sie einige Sekunden. Ihre Eingabe wird geprüft. '
+where fk_id_customItemSet = @customItemSetSMS and pageTypes = @currentPageType and ordinal = @ordinal;
+
+SET @ordinal = 14;
+update CustomItem set value = 'Die Zahlung wurde abgebrochen.'
+where fk_id_customItemSet = @customItemSetSMS and pageTypes = @currentPageType and ordinal = @ordinal;
+
+SET @ordinal = 15;
+update CustomItem set value = 'Starten Sie den Bezahlvorgang erneut, wenn Sie die Zahlung durchführen möchten.'
+where fk_id_customItemSet = @customItemSetSMS and pageTypes = @currentPageType and ordinal = @ordinal;
