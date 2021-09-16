f2aa8b38b8362d178cecfd773b48d7fba9de854f
Emma Abgaryan <emmaa@tce.loc>
2021-09-16 15:14:02 +0400

    [EWLITAGEPG-1975] Removed unnecessary part of Rollback script

--- acs-delivery-scripts/SCRIPTS/RCI_AND_RCE/U5G/COB/Rollback_03_EWLITAGEPG-1975.sql
+++ acs-delivery-scripts/SCRIPTS/RCI_AND_RCE/U5G/COB/Rollback_03_EWLITAGEPG-1975.sql
@@ -2,12 +2,6 @@ USE U5G_ACS_BO;
 
 SET @createdBy = 'A758582';
 
-SET @MaestroVID = NULL;
-SET @MaestroVName = (SELECT `name` FROM `Network` WHERE `code` = 'VISA');
-
-SET @updateState =  'PUSHED_TO_CONFIG';
-SET @currentPageType = 'ALL';
-SET @currentAuthentMean = 'REFUSAL';
 
 SET @customItemSetREFUSAL_COB = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_COB_MISSING_AUTHENT_REFUSAL');
 SET @customItemSetREFUSAL_COZ = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_COZ_MISSING_AUTHENT_REFUSAL');
