e32eb6416095b26c9c6b0389a40f3e0781d8fb1f
Gevorg Aghajanyan <gevorga@energizeglobal.com>
2021-09-24 14:30:08 +0400

    [EWLACS3CDT-180]-update script added 16600_COMDIRECT_PHOTOTAN_02 profile

--- acs-delivery-scripts/SCRIPTS/RCI_AND_RCE/U5G/16600_Comdirect/Rollback_01_EWLACS3CDT-180.sql
+++ acs-delivery-scripts/SCRIPTS/RCI_AND_RCE/U5G/16600_Comdirect/Rollback_01_EWLACS3CDT-180.sql
@@ -2,4 +2,6 @@ USE U5G_ACS_BO;
 
 UPDATE Profile SET dataEntryFormat = '8:(:DIGIT:1)' where name = '16600_COMDIRECT_PHOTOTAN_01';
 
+UPDATE Profile SET dataEntryFormat = '8:(:DIGIT:1)' where name = '16600_COMDIRECT_PHOTOTAN_02';
+
 UPDATE Profile SET dataEntryFormat = '6:(:DIGIT:1)' where name = '16600_COMDIRECT_UNDEFINED';
