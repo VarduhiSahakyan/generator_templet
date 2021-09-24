52d267b736e05d1c168cbb759effab24ed768ba1
Gevorg Aghajanyan <gevorga@energizeglobal.com>
2021-09-24 11:15:22 +0400

    [EWLACS3CDT-180]-Extend photoTAN Entry Field from 6 to 7 digits

---
+++ acs-delivery-scripts/SCRIPTS/RCI_AND_RCE/U5G/16600_Comdirect/01_EWLACS3CDT-180.sql
@@ -0,0 +1,6 @@
+USE U5G_ACS_BO;
+
+
+UPDATE Profile SET dataEntryFormat = '7:(:DIGIT:1)' where name = '16600_COMDIRECT_PHOTOTAN_01';
+
+UPDATE Profile SET dataEntryFormat = '7:(:DIGIT:1)' where name = '16600_COMDIRECT_UNDEFINED';
