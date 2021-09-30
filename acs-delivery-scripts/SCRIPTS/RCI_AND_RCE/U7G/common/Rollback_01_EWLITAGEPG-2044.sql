f7a318dc60214c34a9a06725efc1b5b61ac838c0
Emma Abgaryan <emmaa@tce.loc>
2021-09-30 15:02:55 +0400

    [EWLITAGEPG-2044] Cardholder Info Text

---
+++ acs-delivery-scripts/SCRIPTS/RCI_AND_RCE/U7G/common/Rollback_01_EWLITAGEPG-2044.sql
@@ -0,0 +1,19 @@
+USE U7G_ACS_BO;
+
+SET @createdBy = 'A758582';
+
+
+SET @customItemSetREFUSAL_UBS = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_UBS_MISSING_AUTHENTICATION_REFUSAL');
+SET @customItemSetREFUSAL_SWISSKEY = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_SWISSKEY_MISSING_AUTHENTICATION_REFUSAL');
+SET @customItemSetREFUSAL_RCH = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_RCH_MISSING_AUTHENTICATION_REFUSAL');
+SET @customItemSetREFUSAL_ZKB = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_ZKB_MISSING_AUTHENTICATION_REFUSAL');
+SET @customItemSetREFUSAL_BCV = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_BCV_MISSING_AUTHENTICATION_REFUSAL');
+
+
+DELETE FROM `CustomItem` WHERE `fk_id_customItemSet` IN (@customItemSetREFUSAL_UBS,
+                                                         @customItemSetREFUSAL_SWISSKEY,
+                                                         @customItemSetREFUSAL_RCH,
+                                                         @customItemSetREFUSAL_ZKB,
+                                                         @customItemSetREFUSAL_BCV) AND
+                                            `ordinal` IN (1000, 1001, 1002);
+
