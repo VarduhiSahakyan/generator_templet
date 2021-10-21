287d30093332889d21dea4168e106641c89999d1
Vahe Hovsepyan <vaheh@tce.loc>
2021-10-21 17:26:47 +0400

    [EWLITAGEPG-2044] added cardholder info text for Swisskey Unified profile

--- acs-delivery-scripts/SCRIPTS/RCI_AND_RCE/U7G/common/01_EWLITAGEPG-2044.sql
+++ acs-delivery-scripts/SCRIPTS/RCI_AND_RCE/U7G/common/01_EWLITAGEPG-2044.sql
@@ -72,6 +72,37 @@ INSERT INTO `CustomItem` (`DTYPE`, `createdBy`, `creationDate`, `description`, `
  'it', 1002, @currentPageType, 'La transazione non è andata a buon fine. Si rivolga alla sua banca per registrare la sua carta al servizio 3D Secure.', @MaestroVID, NULL, @customItemSetREFUSAL);
 
 
+SET @customItemSetREFUSAL = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_SWISSKEY_UNIFIED_MISSING_AUTHENTICATION_REFUSAL');
+
+INSERT INTO `CustomItem` (`DTYPE`, `createdBy`, `creationDate`, `description`, `lastUpdateBy`, `lastUpdateDate`,
+                          `name`, `updateState`, `locale`, `ordinal`, `pageTypes`, `value`,
+                          `fk_id_network`, `fk_id_image`, `fk_id_customItemSet`) VALUES
+('T', @createdBy, NOW(), NULL, NULL, NULL, CONCAT(@MaestroVName,'_',@currentAuthentMean,'_',@currentPageType,'_1000'), @updateState,
+ 'de', 1000, @currentPageType, 'Die Zahlung kann nicht ausgeführt werden. Bitte kontaktieren Sie Ihre Bank, um Ihre Karte für 3D Secure zu registrieren.', @MaestroVID, NULL, @customItemSetREFUSAL),
+('T', @createdBy, NOW(), NULL, NULL, NULL, CONCAT(@MaestroVName,'_',@currentAuthentMean,'_',@currentPageType,'_1001'), @updateState,
+ 'de', 1001, @currentPageType, 'Die Zahlung kann nicht ausgeführt werden. Bitte kontaktieren Sie Ihre Bank, um Ihre Karte für 3D Secure zu registrieren.', @MaestroVID, NULL, @customItemSetREFUSAL),
+ ('T', @createdBy, NOW(), NULL, NULL, NULL, CONCAT(@MaestroVName,'_',@currentAuthentMean,'_',@currentPageType,'_1002'), @updateState,
+ 'de', 1002, @currentPageType, 'Die Zahlung kann nicht ausgeführt werden. Bitte kontaktieren Sie Ihre Bank, um Ihre Karte für 3D Secure zu registrieren.', @MaestroVID, NULL, @customItemSetREFUSAL),
+('T', @createdBy, NOW(), NULL, NULL, NULL, CONCAT(@MaestroVName,'_',@currentAuthentMean,'_',@currentPageType,'_1000'), @updateState,
+ 'en', 1000, @currentPageType, 'The payment cannot be processed. Please contact your bank to register your card for 3D Secure.', @MaestroVID, NULL, @customItemSetREFUSAL),
+('T', @createdBy, NOW(), NULL, NULL, NULL, CONCAT(@MaestroVName,'_',@currentAuthentMean,'_',@currentPageType,'_1001'), @updateState,
+ 'en', 1001, @currentPageType, 'The payment cannot be processed. Please contact your bank to register your card for 3D Secure.', @MaestroVID, NULL, @customItemSetREFUSAL),
+ ('T', @createdBy, NOW(), NULL, NULL, NULL, CONCAT(@MaestroVName,'_',@currentAuthentMean,'_',@currentPageType,'_1002'), @updateState,
+ 'en', 1002, @currentPageType, 'The payment cannot be processed. Please contact your bank to register your card for 3D Secure.', @MaestroVID, NULL, @customItemSetREFUSAL),
+('T', @createdBy, NOW(), NULL, NULL, NULL, CONCAT(@MaestroVName,'_',@currentAuthentMean,'_',@currentPageType,'_1000'), @updateState,
+ 'fr', 1000, @currentPageType, 'La transaction n''a pas abouti. Veuillez vous adresser à votre banque pour enregistrer votre carte pour 3D Secure.', @MaestroVID, NULL, @customItemSetREFUSAL),
+('T', @createdBy, NOW(), NULL, NULL, NULL, CONCAT(@MaestroVName,'_',@currentAuthentMean,'_',@currentPageType,'_1001'), @updateState,
+ 'fr', 1001, @currentPageType, 'La transaction n''a pas abouti. Veuillez vous adresser à votre banque pour enregistrer votre carte pour 3D Secure.', @MaestroVID, NULL, @customItemSetREFUSAL),
+ ('T', @createdBy, NOW(), NULL, NULL, NULL, CONCAT(@MaestroVName,'_',@currentAuthentMean,'_',@currentPageType,'_1002'), @updateState,
+ 'fr', 1002, @currentPageType, 'La transaction n''a pas abouti. Veuillez vous adresser à votre banque pour enregistrer votre carte pour 3D Secure.', @MaestroVID, NULL, @customItemSetREFUSAL),
+('T', @createdBy, NOW(), NULL, NULL, NULL, CONCAT(@MaestroVName,'_',@currentAuthentMean,'_',@currentPageType,'_1000'), @updateState,
+ 'it', 1000, @currentPageType, 'La transazione non è andata a buon fine. Si rivolga alla sua banca per registrare la sua carta al servizio 3D Secure.', @MaestroVID, NULL, @customItemSetREFUSAL),
+('T', @createdBy, NOW(), NULL, NULL, NULL, CONCAT(@MaestroVName,'_',@currentAuthentMean,'_',@currentPageType,'_1001'), @updateState,
+ 'it', 1001, @currentPageType, 'La transazione non è andata a buon fine. Si rivolga alla sua banca per registrare la sua carta al servizio 3D Secure.', @MaestroVID, NULL, @customItemSetREFUSAL),
+ ('T', @createdBy, NOW(), NULL, NULL, NULL, CONCAT(@MaestroVName,'_',@currentAuthentMean,'_',@currentPageType,'_1002'), @updateState,
+ 'it', 1002, @currentPageType, 'La transazione non è andata a buon fine. Si rivolga alla sua banca per registrare la sua carta al servizio 3D Secure.', @MaestroVID, NULL, @customItemSetREFUSAL);
+
+
 
 SET @customItemSetREFUSAL = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_RCH_MISSING_AUTHENTICATION_REFUSAL');
 
--- acs-delivery-scripts/SCRIPTS/RCI_AND_RCE/U7G/common/Rollback_01_EWLITAGEPG-2044.sql
+++ acs-delivery-scripts/SCRIPTS/RCI_AND_RCE/U7G/common/Rollback_01_EWLITAGEPG-2044.sql
@@ -5,6 +5,7 @@ SET @createdBy = 'A758582';
 
 SET @customItemSetREFUSAL_UBS = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_UBS_MISSING_AUTHENTICATION_REFUSAL');
 SET @customItemSetREFUSAL_SWISSKEY = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_SWISSKEY_MISSING_AUTHENTICATION_REFUSAL');
+SET @customItemSetREFUSAL_SWISSKEY_Unified = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_SWISSKEY_UNIFIED_MISSING_AUTHENTICATION_REFUSAL');
 SET @customItemSetREFUSAL_RCH = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_RCH_MISSING_AUTHENTICATION_REFUSAL');
 SET @customItemSetREFUSAL_ZKB = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_ZKB_MISSING_AUTHENTICATION_REFUSAL');
 SET @customItemSetREFUSAL_BCV = (SELECT id FROM `CustomItemSet` WHERE `name` = 'customitemset_BCV_MISSING_AUTHENTICATION_REFUSAL');
@@ -12,6 +13,7 @@ SET @customItemSetREFUSAL_BCV = (SELECT id FROM `CustomItemSet` WHERE `name` = '
 
 DELETE FROM `CustomItem` WHERE `fk_id_customItemSet` IN (@customItemSetREFUSAL_UBS,
                                                          @customItemSetREFUSAL_SWISSKEY,
+                                                         @customItemSetREFUSAL_SWISSKEY_Unified,
                                                          @customItemSetREFUSAL_RCH,
                                                          @customItemSetREFUSAL_ZKB,
                                                          @customItemSetREFUSAL_BCV) AND
