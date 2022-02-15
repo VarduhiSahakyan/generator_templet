df23d48e205d186dc3f10ac79fa3fef40a988181
Bruno Gallet <bruno.gallet@worldline.com>
2022-02-15 16:10:14 +0100

    EWLITAGEPG-2274: Update Flash/Babel URLs in Production for ACS

--- acs-delivery-scripts/SCRIPTS/PRD/U2M/EWLITAGEPG-2274-update-flash_url_prod_u7g.sql
+++
@@ -1,8 +0,0 @@
-USE `U2M_ACS_MONITOR`;
-SET @acsId = "U7G";
-
-SET @serviceNameFlash = "flash";
-SET @oldUrlFlash = 'ssl-prd-flash.as8677.net';
-SET @newUrlFlash = 'ssl-prd-flash.wlp-acs.com';
-
-UPDATE `SERVICE_INFO` SET ADDITIONAL_INFO = REPLACE(ADDITIONAL_INFO, @oldUrlFlash, @newUrlFlash) WHERE ACS_ID = @acsID AND SERVICE_NAME = @serviceNameFlash;
--- acs-delivery-scripts/SCRIPTS/PRD/U2M/Rollback_EWLITAGEPG-2274-update-babel_url_prod_any.sql
+++
@@ -1,8 +0,0 @@
-USE `U2M_ACS_MONITOR`;
-SET @acsId = "ANY";
-
-SET @serviceNameBabel = "%babel%";
-SET @oldUrlBabel = 'ssl-prd-babel.as8677.net';
-SET @newUrlBabel = 'ssl-prd-babel.wlp-acs.com';
-
-UPDATE `SERVICE_INFO` SET ADDITIONAL_INFO = REPLACE(ADDITIONAL_INFO, @newUrlBabel, @oldUrlBabel) WHERE ACS_ID = @acsID AND SERVICE_NAME LIKE @serviceNameBabel;
