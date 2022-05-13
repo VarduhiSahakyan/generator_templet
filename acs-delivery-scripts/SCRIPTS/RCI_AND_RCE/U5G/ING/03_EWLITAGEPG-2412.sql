4b59bdc2f99f9dd6e01c0c63147c36b45a11e04e
vahem <w100851>
2022-05-13 15:36:29 +0400

    EWLITAGEPG-2412 ING Setup Maintenance Mode ACS Configuration update

--- acs-delivery-scripts/SCRIPTS/RCI_AND_RCE/U5G/ING/03_EWLITAGEPG-2412.sql
+++ acs-delivery-scripts/SCRIPTS/RCI_AND_RCE/U5G/ING/03_EWLITAGEPG-2412.sql
@@ -6,14 +6,46 @@ SET @updateState =	'PUSHED_TO_CONFIG';
 SET @authMeanSmsExt = (SELECT id FROM `AuthentMeans` WHERE `name` = 'OTP_SMS_EXT_MESSAGE');
 SET @authMeanPhotoTan = (SELECT id FROM `AuthentMeans` WHERE `name` = 'PHOTO_TAN');
 SET @authMeanAttempt = (SELECT id FROM `AuthentMeans` WHERE `name` = 'ATTEMPT');
-SET @authMeanITan = (SELECT id FROM `AuthentMeans` WHERE `name` = 'I_TAN');
 SET @authMeanMobileApp = (SELECT id FROM `AuthentMeans` WHERE `name` = 'MOBILE_APP');
-SET @authMeanPassword = (SELECT id FROM `AuthentMeans` WHERE `name` = 'PASSWORD');
+SET @authMeanPassword = (SELECT id FROM `AuthentMeans` WHERE `name` = 'EXT_PASSWORD');
 SET @subIssuerID = (SELECT id FROM SubIssuer WHERE name = 'ING' AND code = 16500);
 SET @defaultRefusal = (SELECT id FROM Profile WHERE name = '16500_REFUSAL_01' AND fk_id_subIssuer = @subIssuerID);
+SET @issuerID = (SELECT id FROM Issuer WHERE name = 'ING' AND code = 16500);
 
 
 SET @profileACCEPTMAINT = (SELECT id FROM `Profile` WHERE `name` = CONCAT('16500_ACCEPT'));
+
+SET @availableAuthMean = 'MOBILE_APP_EXT|PHOTO_TAN|OTP_SMS_EXT_MESSAGE|I_TAN|REFUSAL|UNDEFINED|EXT_PASSWORD|ATTEMPT';
+
+UPDATE Issuer SET availaibleAuthentMeans = @availableAuthMean WHERE id = @issuerID;
+
+SET @subissuerAuthMean = '[ {
+  "authentMeans" : "REFUSAL",
+  "validate" : true
+}, {
+  "authentMeans" : "UNDEFINED",
+  "validate" : true
+}, {
+  "authentMeans" : "MOBILE_APP_EXT",
+  "validate" : true
+}, {
+  "authentMeans" : "PHOTO_TAN",
+  "validate" : true
+}, {
+  "authentMeans" : "OTP_SMS_EXT_MESSAGE",
+  "validate" : true
+}, {
+  "authentMeans" : "I_TAN",
+  "validate" : true
+}, {
+  "authentMeans" : "EXT_PASSWORD",
+  "validate" : true
+}, {
+  "authentMeans" : "ATTEMPT",
+  "validate" : false
+} ]';
+
+UPDATE SubIssuer SET authentMeans = @subissuerAuthMean WHERE id = @subIssuerID;
 
 UPDATE Rule SET orderRule = 10 WHERE name = 'REFUSAL (DEFAULT)' and fk_id_profile = @defaultRefusal;
 
@@ -40,11 +72,6 @@ WHERE c.`name` = CONCAT('C1_P_', @BankUB, '_01_ACCEPT')
 AND mps.`fk_id_authentMean` = @authMeanPhotoTan
 AND (mps.`meansProcessStatusType` = 'MEANS_DISABLED' AND mps.`reversed` = FALSE);
 
-INSERT INTO `Condition_MeansProcessStatuses` (`id_condition`, `id_meansProcessStatuses`)
-SELECT c.id, mps.id FROM `RuleCondition` c, `MeansProcessStatuses` mps
-WHERE c.`name` = CONCAT('C1_P_', @BankUB, '_01_ACCEPT')
-AND mps.`fk_id_authentMean` = @authMeanITan
-AND (mps.`meansProcessStatusType` = 'MEANS_DISABLED' AND mps.`reversed` = FALSE);
 
 INSERT INTO `Condition_MeansProcessStatuses` (`id_condition`, `id_meansProcessStatuses`)
 SELECT c.id, mps.id FROM `RuleCondition` c, `MeansProcessStatuses` mps
--- acs-delivery-scripts/SCRIPTS/RCI_AND_RCE/U5G/ING/Rollback_03_EWLITAGEPG-2412.sql
+++ acs-delivery-scripts/SCRIPTS/RCI_AND_RCE/U5G/ING/Rollback_03_EWLITAGEPG-2412.sql
@@ -6,6 +6,7 @@ SET @defaultRefusal = (SELECT id FROM Profile WHERE name = '16500_REFUSAL_01' AN
 SET @profileACCEPTMAINT = (SELECT id FROM `Profile` WHERE `name` = CONCAT('16500_ACCEPT'));
 SET @ruleAcceptMaint = (SELECT id FROM `Rule` WHERE `name` = 'ACCEPT MAINT' AND `fk_id_profile` = @profileACCEPTMAINT);
 SET @ruleConditionId = (SELECT id FROM RuleCondition WHERE name = CONCAT('C1_P_', @BankUB, '_01_ACCEPT') AND fk_id_rule = @ruleAcceptMaint);
+SET @issuerID = (SELECT id FROM Issuer WHERE name = 'ING' AND code = 16500);
 
 
 DELETE FROM ProfileSet_Rule WHERE id_rule = @ruleAcceptMaint;
@@ -14,3 +15,33 @@ DELETE FROM  RuleCondition WHERE name = CONCAT('C1_P_', @BankUB, '_01_ACCEPT') A
 DELETE FROM Rule WHERE description = '16500_ACCEPT_MAINTANCE_NORMAL' AND fk_id_profile = @profileACCEPTMAINT;
 
 UPDATE Rule SET orderRule = 9 WHERE name = 'REFUSAL (DEFAULT)' and fk_id_profile = @defaultRefusal;
+
+
+SET @availableAuthMean = 'MOBILE_APP_EXT|PHOTO_TAN|OTP_SMS_EXT_MESSAGE|I_TAN|REFUSAL|UNDEFINED|EXT_PASSWORD';
+
+UPDATE Issuer SET availaibleAuthentMeans = @availableAuthMean WHERE id = @issuerID;
+
+SET @subissuerAuthMean = '[ {
+  "authentMeans" : "REFUSAL",
+  "validate" : true
+}, {
+  "authentMeans" : "UNDEFINED",
+  "validate" : true
+}, {
+  "authentMeans" : "MOBILE_APP_EXT",
+  "validate" : true
+}, {
+  "authentMeans" : "PHOTO_TAN",
+  "validate" : true
+}, {
+  "authentMeans" : "OTP_SMS_EXT_MESSAGE",
+  "validate" : true
+}, {
+  "authentMeans" : "I_TAN",
+  "validate" : true
+}, {
+  "authentMeans" : "EXT_PASSWORD",
+  "validate" : true
+} ]';
+
+UPDATE SubIssuer SET authentMeans = @subissuerAuthMean WHERE fk_id_issuer = @subIssuerID;
