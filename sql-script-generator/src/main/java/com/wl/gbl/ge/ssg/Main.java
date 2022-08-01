142a91f1152a6bc675a5e52d3c70bda9a24c5b66
a117880 <jean-luc.dugardin@worldline.com>
2022-08-01 16:16:47 +0200

    add hub generation scripts

--- sql-script-generator/src/main/java/com/wl/gbl/ge/ssg/Main.java
+++ sql-script-generator/src/main/java/com/wl/gbl/ge/ssg/Main.java
@@ -8,7 +8,8 @@ public class Main {
 		GenerateFile generateFile = new GenerateFile();
 		if(args.length==0) {
 			generateFile.generateFile("/ACS_BO_PROD.properties", "/ACS_BO_script_template_v3.sql", "result.sql");
-			generateFile.generateFileJinjaFromClasspath("/ACS_BO_PROD.json", "/ACS_BO_script_template.j2", "resultJinJa.sql");
+			generateFile.generateFileJinjaFromClasspath("/ACS_HUB_TRIODOS_IAT.json", "/ACS_CR_H5G_script_template.j2", "ACS_CR_H5G_TRIODOS.sql");
+			generateFile.generateFileJinjaFromClasspath("/ACS_HUB_TRIODOS_IAT.json", "/ACS_ASR_script_template.j2" , "ACS_ASR_TRIODOS.sql");
 		}else if(args.length==3){
 			generateFile.generateFileJinjaFromLocal(args[0], args[1], args[2]);
 		}
